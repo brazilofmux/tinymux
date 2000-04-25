12ed6cd4d92c2a16a450b563102bd7c0af3fc06e
Brazil <brazilofmux@gmail.com>
2000-04-25 00:30:32 +0000

    *** empty log message ***

---
+++ src/sa-attrcache.cpp
@@ -0,0 +1,7 @@
+/*
+ * $Id: sa-attrcache.cpp,v 1.1 2000-04-25 00:31:29 sdennis Exp $ 
+ */
+
+#undef MEMORY_BASED
+#define STANDALONE
+#include "attrcache.cpp"
--- src/sa-svdocache.cpp
+++
@@ -1,7 +0,0 @@
-/*
- * $Id: sa-svdocache.cpp,v 1.1 2000-04-11 07:14:47 sdennis Exp $ 
- */
-
-#undef MEMORY_BASED
-#define STANDALONE
-#include "svdocache.cpp"
