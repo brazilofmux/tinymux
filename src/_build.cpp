f272803674f140d475859e7ef432c93978585b11
Brazil <brazilofmux@gmail.com>
2000-09-18 04:56:25 +0000

    no message

---
+++ src/_build.cpp
@@ -0,0 +1,12 @@
+// MUX 2.0
+// Copyright (C) 1998 through 2000 Solid Vertical Domains, Ltd. All
+// rights not explicitly given are reserved. Permission is given to
+// use this code for building and hosting text-based game servers.
+// Permission is given to use this code for other non-commercial
+// purposes. To use this code for commercial purposes other than
+// building/hosting text-based game servers, contact the author at
+// Stephen Dennis <sdennis@svdltd.com> for another license.
+//
+char szBuildDate[] = __DATE__ " " __TIME__;
+char szBuildNum[] = "278";
+char szBetaNum[] = "12";
