68a1f3d0cfc6df345d491fe79345a0872273b058
Brazil <brazilofmux@gmail.com>
2000-09-18 08:16:16 +0000

    no message

--- src/_build.h
+++ src/_build.h
@@ -1,9 +1,16 @@
 // _build.h
 //
-// $Id: _build.h,v 1.5 2000-09-18 08:03:42 sdennis Exp $
+// $Id: _build.h,v 1.6 2000-09-18 08:16:16 sdennis Exp $
 //
-#include "copyright.h"
-
+// MUX 2.0
+// Copyright (C) 1998 through 2000 Solid Vertical Domains, Ltd. All
+// rights not explicitly given are reserved. Permission is given to
+// use this code for building and hosting text-based game servers.
+// Permission is given to use this code for other non-commercial
+// purposes. To use this code for commercial purposes other than
+// building/hosting text-based game servers, contact the author at
+// Stephen Dennis <sdennis@svdltd.com> for another license.
+//
 #ifndef MUX_BUILD_NUM
 extern char szBuildNum[];
 #define MUX_BUILD_NUM szBuildNum
@@ -14,7 +21,7 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.0.12.278"  // Version number
+#define MUX_VERSION       "2.0.12.279"  // Version number
 #define PATCHLEVEL         0            // Patch sequence number
 #define MUX_RELEASE_DATE  "2000-SEP-18" // Source release date
 
