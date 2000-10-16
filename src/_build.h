43a553f9f290f90f602b48907ad27dcadd6bd2a6
Brazil <brazilofmux@gmail.com>
2000-10-16 15:55:47 +0000

    Let loose the hounds of war.

--- src/_build.h
+++ src/_build.h
@@ -1,6 +1,6 @@
 // _build.h
 //
-// $Id: _build.h,v 1.19 2000-10-16 07:37:23 sdennis Exp $
+// $Id: _build.h,v 1.20 2000-10-16 15:55:47 sdennis Exp $
 //
 // MUX 2.0
 // Copyright (C) 1998 through 2000 Solid Vertical Domains, Ltd. All
@@ -21,8 +21,8 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.0.15.292"  // Version number
+#define MUX_VERSION       "2.0.16.293"  // Version number
 #define PATCHLEVEL         0            // Patch sequence number
 #define MUX_RELEASE_DATE  "2000-OCT-16" // Source release date
 
-#define BETA            1               // Define if a BETA release
+#define BETA               0            // Define if a BETA release
