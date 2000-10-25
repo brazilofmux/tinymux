3377b04052b8d14f350d0e5c593594f60c8ea222
Brazil <brazilofmux@gmail.com>
2000-10-25 04:24:48 +0000

    MUX 2.1.0.1

--- src/_build.h
+++ src/_build.h
@@ -1,6 +1,6 @@
 // _build.h
 //
-// $Id: _build.h,v 1.23 2000-10-24 23:57:55 sdennis Exp $
+// $Id: _build.h,v 1.24 2000-10-25 04:24:48 sdennis Exp $
 //
 // MUX 2.0
 // Copyright (C) 1998 through 2000 Solid Vertical Domains, Ltd. All
@@ -21,7 +21,7 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.0.16.294"  // Version number
+#define MUX_VERSION       "2.1.0.1"  // Version number
 #define PATCHLEVEL         0            // Patch sequence number
 #define MUX_RELEASE_DATE  "2000-OCT-24" // Source release date
 
