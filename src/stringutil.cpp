351cf0092cd270a1ba7cf4f71bfa26e77af12d50
Brazil <brazilofmux@gmail.com>
2000-11-17 23:58:19 +0000

    Improve speed of ATOI conversion routines.

--- src/stringutil.cpp
+++ src/stringutil.cpp
@@ -1,6 +1,6 @@
 // stringutil.cpp -- string utilities
 //
-// $Id: stringutil.cpp,v 1.30 2000-10-27 06:56:40 sdennis Exp $
+// $Id: stringutil.cpp,v 1.31 2000-11-17 23:58:19 sdennis Exp $
 //
 // MUX 2.0
 // Portions are derived from MUX 1.6. Portions are original work.
@@ -2002,34 +2002,62 @@ void safe_i64toa(INT64 val, char *buff, char **bufc, int size)
     safe_copy_buf(temp, n, buff, bufc, size);
 }
 
+char TableATOI[16][10] =
+{
+    {  0,  1,  2,  3,  4,  5,  6,  7,  8,  9},
+    { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19},
+    { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29},
+    { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39},
+    { 40, 41, 42, 43, 44, 45, 46, 47, 48, 49},
+    { 50, 51, 52, 53, 54, 55, 56, 57, 58, 59},
+    { 60, 61, 62, 63, 64, 65, 66, 67, 68, 69},
+    { 70, 71, 72, 73, 74, 75, 76, 77, 78, 79},
+    { 80, 81, 82, 83, 84, 85, 86, 87, 88, 89},
+    { 90, 91, 92, 93, 94, 95, 96, 97, 98, 99}
+};
+
 long Tiny_atol(const char *pString)
 {
-    long sum;
-    int c;
-    int LeadingCharacter;
-    
-    // Leading whitespace
-    //
-    while (Tiny_IsSpace[(unsigned char)*pString])
-        pString++;
-
-    // Possible sign
-    //
-    LeadingCharacter = c = *pString++;
-    if (c == '-' || c == '+')
-    {
-        c = *pString++;
-    }
-    
-    sum = 0;
+    long sum = 0;
+    int LeadingCharacter = 0;
     
     // Convert ASCII digits
     //
-    while (Tiny_IsDigit[(unsigned int)c])
+    unsigned c1;
+    unsigned c0 = pString[0];
+    if (!Tiny_IsDigit[c0])
     {
-        sum = 10 * sum + (c - '0');
-        c = *pString++;
+        while (Tiny_IsSpace[(unsigned char)pString[0]])
+        {
+            pString++;
+        }
+        LeadingCharacter = pString[0];
+        if (  LeadingCharacter == '-'
+           || LeadingCharacter == '+')
+        {
+            pString++;
+        }
+        c0 = pString[0];
+        if (!Tiny_IsDigit[c0])
+        {
+            return 0;
+        }
     }
+
+    do
+    {
+        c1 = pString[1];
+        if (Tiny_IsDigit[c1])
+        {
+            sum = 100 * sum + TableATOI[c0-'0'][c1-'0'];
+            pString += 2;
+        }
+        else
+        {
+            sum = 10 * sum + (c0-'0');
+            break;
+        }
+    } while (Tiny_IsDigit[c0 = pString[0]]);
     
     // Interpret sign
     //
@@ -2042,32 +2070,46 @@ long Tiny_atol(const char *pString)
 
 INT64 Tiny_atoi64(const char *pString)
 {
-    INT64 sum;
-    int c;
-    int LeadingCharacter;
-    
-    // Leading whitespace
-    //
-    while (Tiny_IsSpace[(unsigned char)*pString])
-        pString++;
-
-    // Possible sign
-    //
-    LeadingCharacter = c = *pString++;
-    if (c == '-' || c == '+')
-    {
-        c = *pString++;
-    }
-    
-    sum = 0;
+    INT64 sum = 0;
+    int LeadingCharacter = 0;
     
     // Convert ASCII digits
     //
-    while (Tiny_IsDigit[(unsigned int)c])
+    unsigned c1;
+    unsigned c0 = pString[0];
+    if (!Tiny_IsDigit[c0])
     {
-        sum = 10 * sum + (c - '0');
-        c = *pString++;
+        while (Tiny_IsSpace[(unsigned char)pString[0]])
+        {
+            pString++;
+        }
+        LeadingCharacter = pString[0];
+        if (  LeadingCharacter == '-'
+           || LeadingCharacter == '+')
+        {
+            pString++;
+        }
+        c0 = pString[0];
+        if (!Tiny_IsDigit[c0])
+        {
+            return 0;
+        }
     }
+
+    do
+    {
+        c1 = pString[1];
+        if (Tiny_IsDigit[c1])
+        {
+            sum = 100 * sum + TableATOI[c0-'0'][c1-'0'];
+            pString += 2;
+        }
+        else
+        {
+            sum = 10 * sum + (c0-'0');
+            break;
+        }
+    } while (Tiny_IsDigit[c0 = pString[0]]);
     
     // Interpret sign
     //
