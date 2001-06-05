593d596c0e594b3f6a29510ad0d09b908f28e137
Brazil <brazilofmux@gmail.com>
2001-06-05 23:12:02 +0000

    Save and clear global registers before doing @conformat or @exitformat.

--- src/look.cpp
+++ src/look.cpp
@@ -1,6 +1,6 @@
 // look.cpp -- commands which look at things
 //
-// $Id: look.cpp,v 1.21 2001-06-05 06:46:19 sdennis Exp $
+// $Id: look.cpp,v 1.22 2001-06-05 23:12:02 sdennis Exp $
 //
 // MUX 2.0
 // Portions are derived from MUX 1.6. The WOD_REALMS portion is original work.
@@ -486,10 +486,15 @@ static void look_exits(dbref player, dbref loc, const char *exit_name)
         char *FormatOutput = alloc_lbuf("look_exits.FO");
         tPtr = FormatOutput;
 
+        char *preserve[MAX_GLOBAL_REGS];
+        int preserve_len[MAX_GLOBAL_REGS];
+        save_and_clear_global_regs("look_exits_save", preserve, preserve_len);
+
         TinyExec(FormatOutput, &tPtr, 0, loc, player, 
                 EV_FCHECK | EV_EVAL | EV_TOP,
                 &ExitFormat, &VisibleObjectList, 1);
 
+        restore_global_regs("look_exits_restore", preserve, preserve_len);
         notify(player, FormatOutput);
 
         free_lbuf(FormatOutput);
@@ -638,10 +643,15 @@ static void look_contents(dbref player, dbref loc, const char *contents_name, in
         char* ParameterList[] =
             { VisibleObjectList, ContentsNameScratch };
 
+        char *preserve[MAX_GLOBAL_REGS];
+        int preserve_len[MAX_GLOBAL_REGS];
+        save_and_clear_global_regs("look_contents_save", preserve, preserve_len);
+
         TinyExec(FormatOutput, &tPtr, 0, loc, player,
                 EV_FCHECK | EV_EVAL | EV_TOP,
                 &ContentsFormat, ParameterList, 2);
 
+        restore_global_regs("look_contents_restore", preserve, preserve_len);
         notify(player, FormatOutput);
 
         free_lbuf(FormatOutput);
