701aa63d381c25dbec5b4d01dbd3f40889153ce1
Brazil <brazilofmux@gmail.com>
2001-06-05 06:46:19 +0000

    Save and restore registers around @nameformat.

--- src/look.cpp
+++ src/look.cpp
@@ -1,6 +1,6 @@
 // look.cpp -- commands which look at things
 //
-// $Id: look.cpp,v 1.20 2001-06-05 02:08:56 sdennis Exp $
+// $Id: look.cpp,v 1.21 2001-06-05 06:46:19 sdennis Exp $
 //
 // MUX 2.0
 // Portions are derived from MUX 1.6. The WOD_REALMS portion is original work.
@@ -1047,10 +1047,15 @@ void look_in(dbref player, dbref loc, int key)
         char *FormatOutput = alloc_lbuf("look_name.FO");
         char *tPtr = FormatOutput;
 
+        char *preserve[MAX_GLOBAL_REGS];
+        int preserve_len[MAX_GLOBAL_REGS];
+        save_and_clear_global_regs("look_in_save", preserve, preserve_len);
+
         TinyExec(FormatOutput, &tPtr, 0, loc, player,
                 EV_FCHECK | EV_EVAL | EV_TOP,
                 &NameFormat, 0, 0);
 
+        restore_global_regs("look_in_restore", preserve, preserve_len);
         notify(player, FormatOutput);
 
         free_lbuf(FormatOutput);
