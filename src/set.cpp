9e42211918a0f5daa1624d70d94184cd6173f64f
Brazil <brazilofmux@gmail.com>
2001-06-29 15:13:58 +0000

    Attribute part of Bug #707.

--- src/set.cpp
+++ src/set.cpp
@@ -1,6 +1,6 @@
 // set.cpp -- commands which set parameters.
 //
-// $Id: set.cpp,v 1.9 2001-06-29 14:11:17 sdennis Exp $
+// $Id: set.cpp,v 1.10 2001-06-29 15:13:58 sdennis Exp $
 //
 #include "copyright.h"
 #include "autoconf.h"
@@ -601,6 +601,11 @@ void do_chown(dbref player, dbref cause, int key, char *name, char *newown)
         // An attribute was given, so we worry about changing the owner of the
         // attribute.
         //
+        if (isGarbage(thing))
+        {
+            notify_quiet(player, "You shouldn't be rummaging through the garbage.");
+            return;
+        }
         if (!*newown)
         {
             owner = Owner(thing);
