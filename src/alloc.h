3905af13b72e7415cece210a3e566d3e4fa9980c
Brazil <brazilofmux@gmail.com>
2001-06-29 16:39:07 +0000

    Do #endif style.

--- src/alloc.h
+++ src/alloc.h
@@ -1,5 +1,5 @@
 /* alloc.h - External definitions for memory allocation subsystem */
-/* $Id: alloc.h,v 1.1 2000-04-11 07:14:42 sdennis Exp $ */
+/* $Id: alloc.h,v 1.2 2001-06-29 16:39:07 sdennis Exp $ */
 
 #ifndef M_ALLOC_H
 #define M_ALLOC_H
@@ -26,7 +26,22 @@
 #define SBUF_SIZE   32
 */
 
-#ifndef STANDALONE
+#ifdef STANDALONE
+
+#define alloc_lbuf(s)   (char *)malloc(LBUF_SIZE)
+#define free_lbuf(b)    if (b) free(b)
+#define alloc_mbuf(s)   (char *)malloc(MBUF_SIZE)
+#define free_mbuf(b)    if (b) free(b)
+#define alloc_sbuf(s)   (char *)malloc(SBUF_SIZE)
+#define free_sbuf(b)    if (b) free(b)
+#define alloc_bool(s)   (struct boolexp *)malloc(sizeof(struct boolexp))
+#define free_bool(b)    if (b) free(b)
+#define alloc_qentry(s) (BQUE *)malloc(sizeof(BQUE))
+#define free_qentry(b)  if (b) free(b)
+#define alloc_pcache(s) (PCACHE *)malloc(sizeof(PCACHE)
+#define free_pcache(b)  if (b) free(b)
+
+#else // STANDALONE
 
 extern void FDECL(pool_init, (int, int));
 extern char *   FDECL(pool_alloc, (int, const char *));
@@ -47,21 +62,7 @@ extern void FDECL(list_buftrace, (dbref));
 #define alloc_pcache(s) (PCACHE *)pool_alloc(POOL_PCACHE,s)
 #define free_pcache(b)  pool_free(POOL_PCACHE,((char **)&(b)))
 
-#else
-
-#define alloc_lbuf(s)   (char *)malloc(LBUF_SIZE)
-#define free_lbuf(b)    if (b) free(b)
-#define alloc_mbuf(s)   (char *)malloc(MBUF_SIZE)
-#define free_mbuf(b)    if (b) free(b)
-#define alloc_sbuf(s)   (char *)malloc(SBUF_SIZE)
-#define free_sbuf(b)    if (b) free(b)
-#define alloc_bool(s)   (struct boolexp *)malloc(sizeof(struct boolexp))
-#define free_bool(b)    if (b) free(b)
-#define alloc_qentry(s) (BQUE *)malloc(sizeof(BQUE))
-#define free_qentry(b)  if (b) free(b)
-#define alloc_pcache(s) (PCACHE *)malloc(sizeof(PCACHE)
-#define free_pcache(b)  if (b) free(b)
-#endif
+#endif // STANDALONE
 
 #define safe_copy_chr(src, buff, bufp, nSizeOfBuffer) \
 { \
@@ -79,4 +80,4 @@ extern void FDECL(list_buftrace, (dbref));
 #define safe_mb_str(s,b,p)  safe_copy_str(s,b,p,(MBUF_SIZE-1))
 #define safe_mb_chr(c,b,p)  safe_copy_chr(c,b,p,(MBUF_SIZE-1))
 
-#endif  /* M_ALLOC_H */
+#endif // M_ALLOC_H
