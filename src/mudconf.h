98ca5374f0a025ee2d10d7e6b09838b910707ae6
Brazil <brazilofmux@gmail.com>
2001-06-29 06:59:24 +0000

    Temporarily reverse code from Jake.

--- src/mudconf.h
+++ src/mudconf.h
@@ -1,6 +1,6 @@
 // mudconf.h
 //
-// $Id: mudconf.h,v 1.19 2001-06-28 07:13:09 sdennis Exp $
+// $Id: mudconf.h,v 1.20 2001-06-29 06:59:24 sdennis Exp $
 //
 #ifndef __CONF_H
 #define __CONF_H
@@ -105,7 +105,6 @@ struct confdata
     int     player_listen;  /* Are AxHEAR triggered on players? */
     int     quiet_whisper;  /* Can others tell when you whisper? */
     int     dark_sleepers;  /* Are sleeping players 'dark'? */
-    int     destroy_going_now;  // Does GOING act like DESTROY_OK?
     int     see_own_dark;   /* Do you see your own dark stuff? */
     int     idle_wiz_dark;  /* Do idling wizards get set dark? */
     int     pemit_players;  /* Can you @pemit to faraway players? */
@@ -200,9 +199,6 @@ struct confdata
     char    one_coin[32];   /* name of one coin (ie. "penny") */
     char    many_coins[32]; /* name of many coins (ie. "pennies") */
     int     toad_recipient; /* Default @toad recipient. */
-    char    log_prefix[32]; /* Prefix of logfile name */
-    int     safe_wipe;      // If yes, SAFE flag must be removed to @wipe.
-    int     no_startup;     // If yes, startup attributes aren't processed on load.
 #else
     int     paylimit;   /* getting money gets hard over this much */
     int     digcost;    /* cost of @dig command */
