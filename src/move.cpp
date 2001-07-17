c367e54137a6099fbb2020175286eb19a366c14a
Brazil <brazilofmux@gmail.com>
2001-07-17 15:28:09 +0000

    Bug #710 - Leaving an object that is inside a garbage object may garble
    your location and randomly put the server into an endless loop.

--- src/move.cpp
+++ src/move.cpp
@@ -2,7 +2,7 @@
  * move.c -- Routines for moving about 
  */
 /*
- * $Id: move.cpp,v 1.3 2001-02-07 05:28:51 sdennis Exp $ 
+ * $Id: move.cpp,v 1.4 2001-07-17 15:28:09 sdennis Exp $ 
  */
 
 #include "copyright.h"
@@ -866,23 +866,30 @@ void do_enter(dbref player, dbref cause, int key, char *what)
 
 void do_leave(dbref player, dbref cause, int key)
 {
-    dbref loc;
-    int quiet, oattr, aattr;
+    dbref loc = Location(player);
 
-    loc = Location(player);
-
-    if (!Good_obj(loc) || isRoom(loc) || Going(loc)) {
+    if (  !Good_obj(loc)
+       || isRoom(loc)
+       || Going(loc)
+       || !Has_location(loc)
+       || isGarbage(Location(loc)))
+    {
         notify(player, "You can't leave.");
         return;
     }
-    quiet = 0;
+    int quiet = 0;
     if ((key & MOVE_QUIET) && Controls(player, loc))
+    {
         quiet = HUSH_LEAVE;
-    if (could_doit(player, loc, A_LLEAVE)) {
+    }
+    if (could_doit(player, loc, A_LLEAVE))
+    {
         move_via_generic(player, Location(loc), NOTHING, quiet);
-    } else {
-        oattr = quiet ? 0 : A_OLFAIL;
-        aattr = quiet ? 0 : A_ALFAIL;
+    }
+    else
+    {
+        int oattr = quiet ? 0 : A_OLFAIL;
+        int aattr = quiet ? 0 : A_ALFAIL;
         did_it(player, loc, A_LFAIL, "You can't leave.",
                oattr, NULL, aattr, (char **)NULL, 0);
     }
