f9af4a38a4541925a5970870e8256997f2b91c79
Brazil <brazilofmux@gmail.com>
2001-09-08 18:57:49 +0000

    Update the copyright notice.

--- src/_build.h
+++ src/_build.h
@@ -1,9 +1,9 @@
 // _build.h
 //
-// $Id: _build.h,v 1.43 2001-07-07 02:04:33 sdennis Exp $
+// $Id: _build.h,v 1.44 2001-09-08 18:57:49 sdennis Exp $
 //
 // MUX 2.1
-// Copyright (C) 1998 through 2000 Solid Vertical Domains, Ltd. All
+// Copyright (C) 1998 through 2001 Solid Vertical Domains, Ltd. All
 // rights not explicitly given are reserved. Permission is given to
 // use this code for building and hosting text-based game servers.
 // Permission is given to use this code for other non-commercial
