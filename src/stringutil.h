289e32d742e3efa7d595ceb184e63c3b19c5672c
Brazil <brazilofmux@gmail.com>
2001-09-08 19:02:37 +0000

    Update copyright notice.

--- src/stringutil.h
+++ src/stringutil.h
@@ -1,11 +1,11 @@
 // stringutil.h -- string utilities
 //
-// $Id: stringutil.h,v 1.18 2001-08-24 20:55:18 sdennis Exp $
+// $Id: stringutil.h,v 1.19 2001-09-08 19:02:37 sdennis Exp $
 //
 // MUX 2.0
 // Portions are derived from MUX 1.6. Portions are original work.
 //
-// Copyright (C) 1998 through 2000 Solid Vertical Domains, Ltd. All
+// Copyright (C) 1998 through 2001 Solid Vertical Domains, Ltd. All
 // rights not explicitly given are reserved. Permission is given to
 // use this code for building and hosting text-based game servers.
 // Permission is given to use this code for other non-commercial
