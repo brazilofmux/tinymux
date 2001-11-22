63b42721bc0ee71aa36ac7d6414466bf586ae32d
Brazil <brazilofmux@gmail.com>
2001-11-22 07:05:49 +0000

    Reformat.

--- src/a.cpp
+++ src/a.cpp
@@ -1,6 +1,6 @@
 // a.cpp
 //
-// $Id: a.cpp,v 1.8 2001-11-20 05:17:53 sdennis Exp $
+// $Id: a.cpp,v 1.9 2001-11-22 07:05:49 sdennis Exp $
 //
 // Analyse a MUX database, we chew it in on stdin, extract
 // attributes, and stuff them into a radix tree. We accept a single
@@ -19,9 +19,8 @@
 #include "radix.h"
 #include "_build.h"
 
-/*
- * forward
- */
+// Forward.
+//
 void eatline(void);
 void copyattr(char *buff);
 
@@ -57,32 +56,38 @@ int DCL_CDECL main(int ac, char *av[])
         exit(1);
     }
     max_len = atoi(av[1]);
-    if (max_len <= 0) {
-        fprintf(stderr, "usage: %s <positive max string length>\n",
-            av[0]);
+    if (max_len <= 0)
+    {
+        fprintf(stderr, "usage: %s <positive max string length>\n", av[0]);
         exit(1);
     }
-    if (max_len >= 128) {
+    if (max_len >= 128)
+    {
         fprintf(stderr, "A max string length os over 128 is insane.\n");
         exit(1);
     }
-    switch (max_len) {
+    switch (max_len)
+    {
     case 1:
     case 2:
     case 3:
     case 4:
     case 5:
         break;
+
     case 6:
     case 7:
     case 8:
+
         fprintf(stderr,
             "counting all strings up to %s long is going to be expensive\n",
             av[1]);
         break;
+
     default:
+
         fprintf(stderr,
-        "All strings of length %s? I hope you have some real iron\n",
+            "All strings of length %s? I hope you have some real iron\n",
             av[1]);
         break;
     }
@@ -91,26 +96,35 @@ int DCL_CDECL main(int ac, char *av[])
 
     root->count = 0;
 
-    while ((ch = getchar()) != EOF) {
-        switch (ch) {
+    while ((ch = getchar()) != EOF)
+    {
+        switch (ch)
+        {
         case '>':
+
             eatline();
             copyattr(attr);
             len = strlen(attr);
-            for (i = 0; i < len; i++) {
-                sslen = ((len - i) < max_len ?
-                     (len - i) : max_len);
+            for (i = 0; i < len; i++)
+            {
+                sslen = ((len - i) < max_len ? (len - i) : max_len);
                 memmove(substr, attr + i, sslen);
                 substr[sslen] = '\0';
                 r_insert(&root, substr);
             }
             break;
+
         case '!':
+
             objcnt++;
             if ((objcnt & 0x1ff) == 0)
+            {
                 fprintf(stderr, ".");
+            }
             fflush(stderr);
+
         default:
+
             eatline();
         }
     }
@@ -119,49 +133,48 @@ int DCL_CDECL main(int ac, char *av[])
     return 0;
 }
 
-/*
- * Consume a line up to and including the newline
- */
-
+// Consume a line up to and including the newline.
+//
 void eatline(void)
 {
     int ch;
 
-    while ((ch = getchar()) != '\n') {
+    while ((ch = getchar()) != '\n')
+    {
         if (ch == EOF)
+        {
             break;
+        }
     }
 }
-/*
- * Read in a string on stdin and stuff it into the passed down array, observing
- * * the strange rules for attribute escaping.
- */
 
+// Read in a string on stdin and stuff it into the passed down array,
+// observing the strange rules for attribute escaping.
+//
 void copyattr(char *buff)
 {
     char last;
     int i;
-    char ch = '\0';     /*
-
-                 * anything other than a \r
-                 */
-
+    char ch = '\0'; // Anything other than a '\r'.
     i = 0;
     do {
+
         last = ch;
         ch = getchar();
         buff[i++] = ch;
 
-        /*
-         * Internal newlines are escaped as \r\n
-         */
-
-        if (ch == '\n' && last == '\r') {
+        // Internal newlines are escaped as '\r\n'.
+        //
+        if (  ch == '\n'
+           && last == '\r')
+        {
             buff[i - 2] = ' ';
             i--;
         }
-    } while ((ch != '\n' || last == '\r') && ch != EOF && i < 4000);
-    buff[i - 1] = '\0'; /*
-                 * Whack trailing newline
-                 */
+    } while (  (  ch != '\n'
+               || last == '\r')
+            && ch != EOF
+            && i < 4000);
+
+    buff[i - 1] = '\0'; // Whack trailing newline.
 }
