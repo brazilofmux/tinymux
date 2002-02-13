d1a929d1d44c0a8a190168f34053562715c1b843
Brazil <brazilofmux@gmail.com>
2002-02-13 19:21:41 +0000

    Reformat comments.

--- src/mguests.h
+++ src/mguests.h
@@ -1,6 +1,6 @@
 // mguests.h
 //
-// $Id: mguests.h,v 1.3 2002-02-13 18:57:29 sdennis Exp $
+// $Id: mguests.h,v 1.4 2002-02-13 19:21:41 sdennis Exp $
 //
 
 #ifndef  __MGUESTS_H
@@ -10,27 +10,29 @@
 #include "autoconf.h"
 #include "interface.h"
 
-// Object Oriented Programming. I should be shot!
+// Zenty does OOP.
+//
 class CGuests
 {
- private:
-    dbref *Guests; // Main housing of guest dbrefs
-    int nGuests; // Number of them
-    void GrowGuests(int); // They keep growing and growing
-    int MakeGuestChar(void); // Make the guest character
-    void DestroyGuestChar(dbref); // Destroy the guest character
-    void WipeAttrs(dbref guest); // Wipe all the attrbutes
- public:
-    void ListAll(dbref); // @list guests
-    void StartUp(); // Public handler for Startup
-    char *Create(DESC *d); // Public handler for creation
-    void CleanUp(void); // Public handler for cleaning guests.
+private:
+    dbref *Guests;                 // Main housing of guest dbrefs
+    int   nGuests;                 // Number of them
+    void  GrowGuests(int);         // They keep growing and growing
+    int   MakeGuestChar(void);     // Make the guest character
+    void  DestroyGuestChar(dbref); // Destroy the guest character
+    void  WipeAttrs(dbref guest);  // Wipe all the attrbutes
+
+public:
+    void  ListAll(dbref);          // @list guests
+    void  StartUp();               // Public handler for Startup
+    char  *Create(DESC *d);        // Public handler for creation
+    void  CleanUp(void);           // Public handler for cleaning guests.
 };
 
 extern CGuests Guest;
 
-// Let's have a definable password... prevent idiots from conning to #'s.
-// Pay no attention to the praise I give myself!
-#define GUEST_PASSWORD "ZentyRULEZ"  // You know it baby!
+// Zenty's Not-So-Secret Guest Password.
+//
+#define GUEST_PASSWORD "ZentyRULEZ" // Zenty: You know it baby!
 
 #endif
