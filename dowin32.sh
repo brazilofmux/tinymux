8bf53ccafefc8feace3f2003c7178e637fccdf14
Brazil <brazilofmux@gmail.com>
2002-06-14 08:05:09 +0000

    Setup for Build 5.

--- dowin32.sh
+++ dowin32.sh
@@ -3,9 +3,9 @@
 # REQUIRED: The two ReferenceDir must already exist. They may be created by
 # untaring a previous distribution.
 #
-OldBuild=3
+OldBuild=4
 OldVersion=2.2.0.$OldBuild
-NewBuild=4
+NewBuild=5
 NewVersion=2.2.0.$NewBuild
 
 ChangesDir=mux
