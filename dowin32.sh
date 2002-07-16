b1972d8cb80b7fbe50fa883d83d9305649932629
Brazil <brazilofmux@gmail.com>
2002-07-16 07:05:49 +0000

    Prepare for Build 18.

--- dowin32.sh
+++ dowin32.sh
@@ -3,9 +3,9 @@
 # REQUIRED: The two ReferenceDir must already exist. They may be created by
 # untaring a previous distribution.
 #
-OldBuild=16
+OldBuild=17
 OldVersion=2.2.0.$OldBuild
-NewBuild=17
+NewBuild=18
 NewVersion=2.2.0.$NewBuild
 
 ChangesDir=mux
