e7135cd8d825d1b63dc5a482c55ea1c81c273ba8
Brazil <brazilofmux@gmail.com>
2002-08-08 04:58:11 +0000

    Get ready for 2.2.0.31

--- dounix.sh
+++ dounix.sh
@@ -3,9 +3,9 @@
 # REQUIRED: ReferenceDir must already exist. It may be created by untaring a
 # previous distribution.
 #
-OldBuild=29
+OldBuild=30
 OldVersion=2.2.0.$OldBuild
-NewBuild=30
+NewBuild=31
 NewVersion=2.2.0.$NewBuild
 
 ChangesDir=mux
