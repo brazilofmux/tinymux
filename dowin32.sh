4eef0f283128a50e76c57fea314154eb76f60a6c
Brazil <brazilofmux@gmail.com>
2002-11-28 06:35:52 +0000

    *** empty log message ***

--- dowin32.sh
+++ dowin32.sh
@@ -3,9 +3,9 @@
 # REQUIRED: The two ReferenceDir must already exist. They may be created by
 # untaring a previous distribution.
 #
-OldBuild=46
+OldBuild=47
 OldVersion=2.2.0.$OldBuild
-NewBuild=47
+NewBuild=48
 NewVersion=2.2.0.$NewBuild
 
 ChangesDir=mux
