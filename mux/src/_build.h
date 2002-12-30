50908bb1f5df041c551b03c9a9b738a87fce726c
Brazil <brazilofmux@gmail.com>
2002-12-30 17:27:34 +0000

    *** empty log message ***

--- mux/src/_build.h
+++ mux/src/_build.h
@@ -1,6 +1,6 @@
 // _build.h
 //
-// $Id: _build.h,v 1.48 2002-11-28 06:18:41 sdennis Exp $
+// $Id: _build.h,v 1.49 2002-12-30 17:27:34 sdennis Exp $
 //
 // MUX 2.1
 // Copyright (C) 1998 through 2002 Solid Vertical Domains, Ltd. All
@@ -22,8 +22,8 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.2.0.47"     // Version number
-#define MUX_RELEASE_DATE  "2002-NOV-27"  // Source release date
+#define MUX_VERSION       "2.2.0.48"     // Version number
+#define MUX_RELEASE_DATE  "2002-DEC-30"  // Source release date
 
 // Define if this release is qualified as ALPHA or BETA.
 //
