24ede27f41a0841d1dd1110bfeac22c48ee290e8
Brazil <brazilofmux@gmail.com>
2003-01-04 07:49:02 +0000

    Artistic License.

--- mux/src/copyright.h
+++ mux/src/copyright.h
@@ -1,84 +1,221 @@
 /* copyright.h */
 /* -*-C-*- */
-/* $Id: copyright.h,v 1.1 2002-05-24 06:53:15 sdennis Exp $ */
+/* $Id: copyright.h,v 1.2 2003-01-04 07:49:02 sdennis Exp $ */
 
 /*
- * TinyMUX 2.0 and 2.1 maintained by Stephen Dennis (AKA Brazil)
- * Copyright (C) 1999, 2000, 2001, and 2002 by Solid Vertical Domains, Ltd.
- * Stephen Dennis <sdennis@svdltd.com>
- * 
- * TinyMUX 1.0, 1.1 and 1.2 Source code is maintained by David Passmore
- * (Lauren@Children of the Atom).
- * Comments should be sent to lauren@ranger.range.orst.edu.
- * TinyMUX is Copyright (c) 1995 by David Passmore.
+ * TinyMUX 2.2 Source Code.
+ * Copyright (C) 1999-2003 by Solid Vertical Domains, Ltd.
+ * Contact: Stephen Dennis
  *
- * Many parts of the source code, and the help text were derived from
- * TinyMUSH 2.2:
- * TinyMUSH 2.2 Source was written in part by
- *     Jean Marie Diaz, Lydia Leong, and Devin Hooker
- *     2.2 initial alpha release 9/21/94.
- *         final beta snapshot (hopefully) 3/4/95
+ * TinyMUX Source Code (verions 1.0 through 1.6)
+ * Copyright (c) 1995, 1996, 1997, 1998, 1999, David Passmore.
  *
- * Many parts of the source code, and the help text were derived from
- * PennMUSH 1.50:
+ * TinyMUSH 2.2 Source Code
+ * Copyright (c) 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, Lydia Leong,
+ * Jean Marie Diaz, and Deborah Wilson-Hooker.
  *
- * Patchlevel 12 is based on PennMUSH pl10, the last version of PennMUSH
- * released by Amberyl. All copyright notices above continue to apply.
- * Versions of PennMUSH prior to pl10 are written in part by
- *      Alan Schwartz
- * Thanks and praise are due to:
- * Ralph Melton (Rhyanna@Castle D'Image) for bugs reports, patches, ideas,
- *    and extensions
- * T. Alexander Popiel for bug reports, patches, ideas, and extensions, too.
- * Al Brown (Kalkin@DarkZone) for many clever ideas
- * The many other people who've sent in bug reports
- * The admin and players of Dune II, who put up with a lot of broken code
- * Special thanks to Amberyl for all her help.
- *      - Paul/Javelin (Alan Schwartz, alansz@mellers1.psych.berkeley.edu)
- *
- * Based on TinyMUSH 2.0 Source code
- * Copyright (c) 1991 Joseph Traub and Glenn Crocker
+ * TinyMUSH 2.0 Source code
+ * Copyright (c) 1991, Joseph Traub and Glenn Crocker. All rights reserved.
  *
  * Based on TinyMUD code
- * Copyright (c) 1989, 1990 by David Applegate, James Aspnes, Timothy Freeman,
- *                             and Bennet Yee.
+ * Copyright (c) 1995, David Applegate, James Aspnes, Timothy Freeman,
+ *                     and Bennet Yee. All rights reserved.
+ * (Modified by these authors from the original 1989, 1990 TinyMUD copyright.)
  *
- * This material was developed by the above-mentioned authors.  Permission to
- * copy this software, to redistribute it, and to use it for any purpose is
- * granted, subject to the following restrictions and understandings.
  *
- * 1. Any copy made of this software must include this copyright notice in
- *    full.
  *
- * 2. Users of this software agree to make their best efforts
- *    (a) to return to the above-mentioned authors any improvements or
- *        extensions that they make, so that these may be included in future
- *        releases; and
- *    (b) to inform the authors of noteworthy uses of this software.
+ * TinyMUX 2.0 is a derivation of TinyMUX 1.6 which is a derivation of
+ * TinyMUSH 2.0.10 patchlevel 6.  TinyMUSH 2.0 is dervived from TinyMUD.
  *
- * 3. All materials developed as a consequence of the use of this software
- *    shall duly acknowledge such use, in accordance with the usual standards
- *    of acknowledging credit in academic research.
+ * There are also portions of source code derived from PennMUSH 1.50.
  *
- * 4. The authors have made no warrantee or representation that the operation
- *    of this software will be error-free, and the authors are under no
- *    obligation to provide any services, by way of maintenance, update, or
- *    otherwise.
+ * The TinyMUD copyright follows below.  PennMUSH, TinyMUSH 2.0 and
+ * TinyMUX 1.6 are now released under the OSI-standard Artistic License.
  *
- * 5. In conjunction with products arising from the use of this material,
- *    there shall be no use of the names of the authors, of Carnegie-Mellon
- *    University, nor of any adaptation thereof in any advertising,
- *    promotional, or sales literature without prior written consent from
- *    the authors and both Carnegie-Mellon University Case Wester Reserve
- *    University in each case.
  *
- * Credits:
+ *
+ * Revised TinyMUD copyright:
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that: (1) source code distributions
+ * retain the above copyright notice and this paragraph in its entirety,
+ * and (2) distributions including binary code include the above copyright
+ * notice and this paragraph in its entirety in the documentation or other
+ * materials provided with the distribution.  The names of the copyright
+ * holders may not be used to endorse or promote products derived from 
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ *
+ * TinyMUX 2.2 Copyright: The Artistic License
+ *
+ * Preamble
+ *
+ * The intent of this document is to state the conditions under which a
+ * Package may be copied, such that the Copyright Holder maintains some
+ * semblance of artistic control over the development of the package,
+ * while giving the users of the package the right to use and distribute
+ * the Package in a more-or-less customary fashion, plus the right to
+ * make reasonable modifications.
+ *
+ * Definitions:
+ *     - "Package" refers to the collection of files distributed by the
+ *       Copyright Holder, and derivatives of that collection of files
+ *       created through textual modification.
+ *     - "Standard Version" refers to such a Package if it has not been
+ *       modified, or has been modified in accordance with the wishes of
+ *       the Copyright Holder.
+ *     - "Copyright Holder" is whoever is named in the copyright or
+ *       copyrights for the package.
+ *     - "You" is you, if you're thinking about copying or distributing
+ *       this Package.
+ *     - "Reasonable copying fee" is whatever you can justify on the basis
+ *       of media cost, duplication charges, time of people involved, and
+ *       so on. (You will not be required to justify it to the Copyright
+ *       Holder, but only to the computing community at large as a market
+ *       that must bear the fee.)
+ *     - "Freely Available" means that no fee is charged for the item
+ *       itself, though there may be fees involved in handling the item. It
+ *       also means that recipients of the item may redistribute it under
+ *       the same conditions they received it.
+ *
+ * 1. You may make and give away verbatim copies of the source form of
+ * the Standard Version of this Package without restriction, provided
+ * that you duplicate all of the original copyright notices and
+ * associated disclaimers.
+ *
+ * 2. You may apply bug fixes, portability fixes and other modifications
+ * derived from the Public Domain or from the Copyright Holder. A Package
+ * modified in such a way shall still be considered the Standard Version.
+ *
+ * 3. You may otherwise modify your copy of this Package in any way,
+ * provided that you insert a prominent notice in each changed file
+ * stating how and when you changed that file, and provided that you do
+ * at least ONE of the following:
+ *
+ *   a) place your modifications in the Public Domain or otherwise make
+ *   them Freely Available, such as by posting said modifications to
+ *   Usenet or an equivalent medium, or placing the modifications on a
+ *   major archive site such as ftp.uu.net, or by allowing the Copyright
+ *   Holder to include your modifications in the Standard Version of the
+ *   Package.
+ *
+ *   b) use the modified Package only within your corporation or
+ *   organization.
+ *
+ *   c) rename any non-standard executables so the names do not conflict
+ *   with standard executables, which must also be provided, and provide
+ *   a separate manual page for each non-standard executable that
+ *   clearly documents how it differs from the Standard Version.
+ *   d) make other distribution arrangements with the Copyright Holder.
+ *
+ * 4. You may distribute the programs of this Package in object code or
+ * executable form, provided that you do at least ONE of the following:
+ *
+ *   a) distribute a Standard Version of the executables and library
+ *   files, together with instructions (in the manual page or
+ *   equivalent) on where to get the Standard Version.
+ *
+ *   b) accompany the distribution with the machine-readable source of
+ *   the Package with your modifications.
+ *
+ *   c) accompany any non-standard executables with their corresponding
+ *   Standard Version executables, giving the non-standard executables
+ *   non-standard names, and clearly documenting the differences in
+ *   manual pages (or equivalent), together with instructions on where
+ *   to get the Standard Version.
+ *
+ *   d) make other distribution arrangements with the Copyright Holder.
+ *
+ * 5. You may charge a reasonable copying fee for any distribution of
+ * this Package. You may charge any fee you choose for support of this
+ * Package. You may not charge a fee for this Package itself. However,
+ * you may distribute this Package in aggregate with other (possibly
+ * commercial) programs as part of a larger (possibly commercial)
+ * software distribution provided that you do not advertise this Package
+ * as a product of your own.
+ *
+ * 6. The scripts and library files supplied as input to or produced as
+ * output from the programs of this Package do not automatically fall
+ * under the copyright of this Package, but belong to whomever generated
+ * them, and may be sold commercially, and may be aggregated with this
+ * Package.
+ *
+ * 7. C or perl subroutines supplied by you and linked into this Package
+ * shall not be considered part of this Package.
+ *
+ * 8. The name of the Copyright Holder may not be used to endorse or
+ * promote products derived from this software without specific prior
+ * written permission.
+ *
+ * 9. THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTIBILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * The author may be reached via email at sdennis@svdltd.com.
+ *
+ *
+ *
+ * TinyMUX 1.6 Credits:
+ *
+ *    James Callahan (Darkenelf), who contributed many patches, and ideas
+ * (@teleport/quiet, @readcache fixes, side effect functions, many bug fixes).
+ *
+ *    Chris (Children of the Atom) for many ideas and many bug finds.
+ *
+ *    Ralph Melton (Rhyanna@Castle D'Image) for bugs reports, patches, ideas,
+ * and extensions.
+ *
+ *    T. Alexander Popiel for bug reports, patches, ideas, and extensions, too.
+ *
+ *    Al Brown (Kalkin@DarkZone) for many clever ideas
+ *
+ *    Kalkin(DarkZone) and Harlock(StarWarsII) who extended the comsystem and
+ * added tons of new commands.
+ *
+ *    Kalkin, again, for the basis of the mail alias and guest code, and the
+ * idea of restarting on a fatal signal.
+ *
+ *    Alan Schwartz (Javelin/Paul) and Lydia Leong (Amberyl), whose extended
+ * mailer from PennMUSH is the basis for MUX's @mail system. Also, Brandy
+ * (CrystalMUSH) whose +mail system inspired many of the features.
+ *
+ *    Ethaniel and Kayan Telva (BTech3056) for the basic comsystem and macro
+ * code.
+ *
+ *    Dreamline(Horizons) who helped update the help text.
+ *
+ *    Airam(Generations) for ideas on the stack and @program code.
+ *
+ *    Mike(StarWars), idea for not saving GOING objects, and mail expiration
+ * code.
+ *
+ *    Dean Gaudet, for his user and hostname slave process code.
+ *
+ *    Andrew Molitor, for the radix compression library, and some wonderful
+ * utilities.
+ *
+ *    Robby Griffin, whose skill in uncovering obscure bugs has saved
+ * everyone a lot of time and effort.
+ *
+ *    Many others.
+ *
+ *
+ *
+ * TinyMUSH 2.0 Credits:
  *    Lawrence Foard:
  *      Wrote the original TinyMUSH 1.0 code from which this later derived.
  *    Jin (and MicroMUSH):
  *      Made many, many changes to the code that improved it immensely.
+ *    Robert Hood (virus):
+ *      Modified the interface.c code to support alots of users.
  *    Lachesis:
  *      Introduced the idea of property lists to TinyMUCK
  *    Many others:
  *      Many features borrowed from other muds.
+ *
  */
