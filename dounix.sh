031ec7b4bab6853935d5db645e20a92d4aa648d7
Brazil <brazilofmux@gmail.com>
2003-02-20 08:44:00 +0000

    *** empty log message ***

--- dounix.sh
+++ dounix.sh
@@ -3,9 +3,9 @@
 # REQUIRED: ReferenceDir must already exist. It may be created by untaring a
 # previous distribution.
 #
-OldBuild=4
+OldBuild=5
 OldVersion=2.3.0.$OldBuild
-NewBuild=5
+NewBuild=6
 NewVersion=2.3.0.$NewBuild
 
 ChangesDir=mux
--- mux/src/_build.h
+++ mux/src/_build.h
@@ -1,6 +1,6 @@
 // _build.h
 //
-// $Id: _build.h,v 1.6 2003-02-15 07:22:49 sdennis Exp $
+// $Id: _build.h,v 1.7 2003-02-20 08:44:00 sdennis Exp $
 //
 // MUX 2.3
 // Copyright (C) 1998 through 2003 Solid Vertical Domains, Ltd. All
@@ -16,8 +16,8 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.3.0.4"      // Version number
-#define MUX_RELEASE_DATE  "2003-FEB-14"  // Source release date
+#define MUX_VERSION       "2.3.0.5"      // Version number
+#define MUX_RELEASE_DATE  "2003-FEB-20"  // Source release date
 
 // Define if this release is qualified as ALPHA or BETA.
 //
