c6ddb21b4e16f868c2e341df8a68bb45648e789d
Brazil <brazilofmux@gmail.com>
2003-03-01 23:17:23 +0000

    Use FALSE instead of 0.

--- src/bsd.cpp
+++ src/bsd.cpp
@@ -1,6 +1,6 @@
 // bsd.cpp
 //
-// $Id: bsd.cpp,v 1.66 2003-02-18 04:34:00 sdennis Exp $
+// $Id: bsd.cpp,v 1.67 2003-03-01 23:17:23 sdennis Exp $
 //
 // MUX 2.1
 // Portions are derived from MUX 1.6 and Nick Gammon's NT IO Completion port
@@ -2788,7 +2788,7 @@ RETSIGTYPE DCL_CDECL sighandler(int sig)
         {
             exit(0);
         }
-        mudstate.dumping = 0;
+        mudstate.dumping = FALSE;
         break;
 
     case SIGHUP:
