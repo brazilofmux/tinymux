0cce4939acdda080e2ed8c2f6844e6107c71ecf7
Brazil <brazilofmux@gmail.com>
2003-05-08 06:24:59 +0000

    Support obj(name(<dbref>)) where name(<dbref>) contains ANSI color.
    Reported by Bellemore.

--- mux/src/functions.cpp
+++ mux/src/functions.cpp
@@ -1,6 +1,6 @@
 // functions.cpp -- MUX function handlers.
 //
-// $Id: functions.cpp,v 1.57 2003-03-03 17:09:03 sdennis Exp $
+// $Id: functions.cpp,v 1.58 2003-05-08 06:24:59 sdennis Exp $
 //
 // MUX 2.3
 // Copyright (C) 1998 through 2003 Solid Vertical Domains, Ltd. All
@@ -5139,7 +5139,7 @@ FUNCTION(fun_nearby)
 
 static void process_sex(dbref player, char *what, const char *token, char *buff, char **bufc)
 {
-    dbref it = match_thing_quiet(player, what);
+    dbref it = match_thing_quiet(player, strip_ansi(what));
     if (!Good_obj(it))
     {
         safe_match_result(it, buff, bufc);
