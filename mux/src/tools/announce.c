5d3de5448d118ddd2470679fb69439c623019ea2
Brazil <brazilofmux@gmail.com>
2004-04-08 18:34:07 +0000

    Clean up announcer.

--- mux/src/tools/announce.c
+++ mux/src/tools/announce.c
@@ -9,7 +9,7 @@
  *      Bits of code are adapted from the Berkeley telnetd sources
  */
 
-#define PORT    4201
+#define PORT    2860
 
 #include <sys/param.h>
 #include <sys/socket.h>
@@ -22,47 +22,62 @@
 #include <stdio.h>
 #include <ctype.h>
 
-extern char **environ;
-extern int errno;
-char   *Name;			/* name of this program for error messages */
-char    msg[32768];
+char   *Name;        // name of this program for error messages.
+char    msg[8192];
+size_t  nmsg;
 
 int main(int argc, char *argv[])
 {
-    int     s, ns, foo;
+    int    s;
+    int    ns;
+    int    foo;
     static struct sockaddr_in sin = {AF_INET};
-    char   *host, *inet_ntoa();
-    char    tmp[32768];
-    long    ct;
+    char   *host;
+    char   *inet_ntoa();
+    long   ct;
 
-    Name = argv[0];		/* save name of program for error messages  */
-    sin.sin_port = htons((u_short) PORT);	/* Assume PORT */
-    argc--, argv++;
+    // Save program name for error messages.
+    //
+    Name = argv[0];
+
+    // Assume PORT, but let command-line override.
+    //
+    sin.sin_port = htons((u_short) PORT);
+    argc--;
+    argv++;
     if (argc > 0)
     {
         // unless specified on command-line.
         //
-	sin.sin_port = atoi(*argv);
-	sin.sin_port = htons((u_short) sin.sin_port);
+        sin.sin_port = atoi(*argv);
+        sin.sin_port = htons((u_short) sin.sin_port);
     }
-    strcpy(msg, "");
-    strcpy(tmp, "");
-    while (1)
+
+    // Read in message and translate CRLF/NL to something reasonable.
+    //
+    int  ch;
+    char *p = msg;
+    while (  (ch = getchar()) != EOF
+          && p + 2 < msg + sizeof(msg))
     {
-	if ((gets(tmp)) == NULL)
+        if (ch != '\r')
         {
-	    break;
+            if (ch == '\n')
+            {
+                *p++ = '\r';
+            }
+            *p++ = ch;
         }
-	strcat(tmp, "\r\n");
-	strcat(msg, tmp);
     }
-    msg[4095] = '\0';
-    signal(SIGHUP, SIG_IGN);	/* get socket, bind port to it      */
+    *p = '\0';
+    nmsg = p - msg;
+
+    signal(SIGHUP, SIG_IGN);
     s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0)
     {
-	perror("announce: socket");
-	exit(1);
+        perror("announce: socket");
+        exit(1);
     }
     int opt = 1;
     if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
@@ -71,44 +86,44 @@ int main(int argc, char *argv[])
     }
     if (bind(s, (struct sockaddr *)&sin, sizeof sin) < 0)
     {
-	perror("bind");
-	exit(1);
+        perror("bind");
+        exit(1);
     }
     if ((foo = fork()) != 0)
     {
-	fprintf(stderr, "announce: pid %d running on port %d\n", foo,
-		ntohs((u_short) sin.sin_port));
-	_exit(0);
+        fprintf(stderr, "announce: pid %d running on port %d\n", foo,
+                ntohs((u_short) sin.sin_port));
+        _exit(0);
     }
     else
     {
-	setpriority(PRIO_PROCESS, getpid(), 10);
+        setpriority(PRIO_PROCESS, getpid(), 10);
     }
     if (listen(s, 1) < 0)
     {
         // start listening on port.
         //
-	perror("announce: listen");
-	_exit(1);
+        perror("announce: listen");
+        _exit(1);
     }
     foo = sizeof sin;
     for (;;)
     {
         // loop forever, accepting requests & printing msg.
         //
-	ns = accept(s, (struct sockaddr *)&sin, &foo);
-	if (ns < 0)
+        ns = accept(s, (struct sockaddr *)&sin, &foo);
+        if (ns < 0)
         {
-	    perror("announce: accept");
-	    _exit(1);
-	}
-	host = inet_ntoa(sin.sin_addr);
-	ct = time(0L);
-	fprintf(stderr, "CONNECTION made from %s at %s",
-		host, ctime(&ct));
-	write(ns, msg, strlen(msg));
-	sleep(5);
-	close(ns);
+            perror("announce: accept");
+            _exit(1);
+        }
+        host = inet_ntoa(sin.sin_addr);
+        ct = time(0L);
+        fprintf(stderr, "CONNECTION made from %s at %s",
+                host, ctime(&ct));
+        write(ns, msg, nmsg);
+        sleep(5);
+        close(ns);
     }
-}				/* main */
+}
 
