8a4970b22acc9e4081006fb65cefd957724726c5
Brazil <brazilofmux@gmail.com>
2004-05-17 19:09:27 +0000

    Bump build numbers.

--- dounix.sh
+++ dounix.sh
@@ -3,9 +3,9 @@
 # REQUIRED: ReferenceDir must already exist. It may be created by untaring a
 # previous distribution.
 #
-OldBuild=3
+OldBuild=4
 OldVersion=2.4.0.$OldBuild
-NewBuild=4
+NewBuild=5
 NewVersion=2.4.0.$NewBuild
 
 ChangesDir=mux
