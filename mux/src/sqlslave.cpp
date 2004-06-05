7c237d55862f1a9fe440fdcf32b1fc7275c1c519
Brazil <brazilofmux@gmail.com>
2004-06-05 17:53:40 +0000

    Add sqlslave.cpp to the build.

---
+++ mux/src/sqlslave.cpp
@@ -0,0 +1,17 @@
+// sqlslave.cpp -- This slave does SQL queries.
+//
+// $Id: sqlslave.cpp,v 1.1 2004-06-05 17:53:40 sdennis Exp $
+//
+#include "autoconf.h"
+#include "config.h"
+
+#ifdef _SGI_SOURCE
+#define CAST_SIGNAL_FUNC (SIG_PF)
+#else
+#define CAST_SIGNAL_FUNC
+#endif
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
