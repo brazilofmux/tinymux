a566e96ffacadcaf67efda1adf3d56f896436104
Brazil <brazilofmux@gmail.com>
2004-07-07 17:48:01 +0000

    Change the parameter names.

--- mux/src/vattr.h
+++ mux/src/vattr.h
@@ -1,12 +1,12 @@
 // vattr.h -- Definitions for user-defined attributes.
 //
-// $Id: vattr.h,v 1.2 2004-07-07 17:00:47 sdennis Exp $
+// $Id: vattr.h,v 1.3 2004-07-07 17:48:01 sdennis Exp $
 //
 
 extern ATTR *vattr_rename_LEN(char *, int, char *, int);
-extern ATTR *vattr_find_LEN(const char *pName, size_t nName);
-extern ATTR *vattr_alloc_LEN(char *pName, size_t nName, int flags);
-extern ATTR *vattr_define_LEN(char *pName, size_t nName, int number, int flags);
+extern ATTR *vattr_find_LEN(const char *pAttrName, size_t nAttrName);
+extern ATTR *vattr_alloc_LEN(char *pAttrName, size_t nAttrName, int flags);
+extern ATTR *vattr_define_LEN(char *pAttrName, size_t nAttrName, int number, int flags);
 extern void  vattr_delete_LEN(char *, int);
 extern ATTR *vattr_first(void);
 extern ATTR *vattr_next(ATTR *);
