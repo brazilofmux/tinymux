d398e8f93e92b42801f1afefa76041ff237360b0
Brazil <brazilofmux@gmail.com>
2004-07-10 03:00:49 +0000

    Prepare for 2.4.0.10

--- dowin32.sh
+++ dowin32.sh
@@ -3,9 +3,9 @@
 # REQUIRED: The two ReferenceDir must already exist. They may be created by
 # untaring a previous distribution.
 #
-OldBuild=8
+OldBuild=9
 OldVersion=2.4.0.$OldBuild
-NewBuild=9
+NewBuild=10
 NewVersion=2.4.0.$NewBuild
 
 ChangesDir=mux
