16c619fc35324c00b0fbe7323328e5988b648e84
Brazil <brazilofmux@gmail.com>
2004-10-29 21:30:08 +0000

    *** empty log message ***

---
+++ mux/src/sqlshared.cpp
@@ -0,0 +1,12 @@
+// sqlshared.cpp -- Shared code between the main server and the sqlslave.
+//
+// $Id: sqlshared.cpp,v 1.1 2004-10-29 21:30:08 sdennis Exp $
+//
+#include "autoconf.h"
+#include "config.h"
+#include "sqlshared.h"
+
+void sqlfoo(void)
+{
+    return;
+}
---
+++ mux/src/sqlshared.h
@@ -0,0 +1,11 @@
+// sqlshared.h -- Shared code between the main server and the sqlslave.
+//
+// $Id: sqlshared.h,v 1.1 2004-10-29 21:30:08 sdennis Exp $
+//
+
+#ifndef SQLSHARED_H
+#define SQLSHARED_H
+
+extern void sqlfoo(void);
+
+#endif // SQLSHARED_H
