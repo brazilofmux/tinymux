b12c56f8c0b92f1cd4f99431740536a3c4114580
Brazil <brazilofmux@gmail.com>
2005-05-26 01:44:17 +0000

    no message

--- mux/src/_build.h
+++ mux/src/_build.h
@@ -1,10 +1,11 @@
 // _build.h
 //
-// $Id: _build.h,v 1.38 2004-12-25 18:35:08 sdennis Exp $
+// $Id: _build.h,v 1.39 2005-05-26 01:44:17 sdennis Exp $
 //
 // MUX 2.4
-// Copyright (C) 1998 through 2004 Solid Vertical Domains, Ltd. All
-// rights not explicitly given are reserved.
+// Copyright (C) 2005 Stephen Dennis. 
+// Copyright (C) 1998 through 2004 Solid Vertical Domains, Ltd.
+// All rights not explicitly given are reserved.
 //
 #ifndef MUX_BUILD_NUM
 extern char szBuildNum[];
@@ -21,8 +22,8 @@ extern char szBuildDate[];
 #else
 #define BT_VER            ""
 #endif
-#define MUX_VERSION       "2.4.0.14" BT_VER  // Version number
-#define MUX_RELEASE_DATE  "2004-DEC-25"      // Source release date
+#define MUX_VERSION       "2.4.0.15" BT_VER  // Version number
+#define MUX_RELEASE_DATE  "2005-MAY-25"      // Source release date
 
 // Define if this release is qualified as ALPHA or BETA.
 //
