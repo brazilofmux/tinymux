a3d3922adfe29e81385f050900db946f234abcf8
Brazil <brazilofmux@gmail.com>
2005-08-07 01:21:41 +0000

    *** empty log message ***

--- dounix.sh
+++ dounix.sh
@@ -3,9 +3,9 @@
 # REQUIRED: ReferenceDir must already exist. It may be created by untaring a
 # previous distribution.
 #
-OldBuild=19
+OldBuild=20
 OldVersion=2.4.0.$OldBuild
-NewBuild=20
+NewBuild=21
 NewVersion=2.4.0.$NewBuild
 
 ChangesDir=mux
