f9be263b99837eb9c7aa1939b6cae5ff7e986e3c
Brazil <brazilofmux@gmail.com>
2005-10-19 15:00:17 +0000

    2.4.0.22

--- mux/src/_build.h
+++ mux/src/_build.h
@@ -1,6 +1,6 @@
 // _build.h
 //
-// $Id: _build.h,v 1.47 2005-10-17 00:08:00 sdennis Exp $
+// $Id: _build.h,v 1.48 2005-10-19 15:00:17 sdennis Exp $
 //
 // MUX 2.4
 // Copyright (C) 2005 Stephen Dennis.
@@ -17,8 +17,8 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.4.0.21"         // Version number
-#define MUX_RELEASE_DATE  "2005-OCT-16"      // Source release date
+#define MUX_VERSION       "2.4.0.22"         // Version number
+#define MUX_RELEASE_DATE  "2005-OCT-19"      // Source release date
 
 // Define if this release is qualified as ALPHA or BETA.
 //
