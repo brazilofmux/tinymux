b71c71e86d280234946afd26466bd5bbc8a83221
Brazil <brazilofmux@gmail.com>
2006-01-02 07:34:42 +0000

    Start using dbi.h and libdbi.

--- mux/src/sqlslave.cpp
+++ mux/src/sqlslave.cpp
@@ -1,9 +1,10 @@
 // sqlslave.cpp -- This slave does SQL queries.
 //
-// $Id: sqlslave.cpp,v 1.2 2005-11-24 20:07:06 sdennis Exp $
+// $Id: sqlslave.cpp,v 1.3 2006-01-02 07:34:42 sdennis Exp $
 //
 #include "autoconf.h"
 #include "config.h"
+#include <dbi/dbi.h>
 
 #define MAX_STRING 1000
 
@@ -18,6 +19,9 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    dbi_conn conn;
+    dbi_initialize(NULL);
+
     for (;;)
     {
         char arg[MAX_STRING];
@@ -29,7 +33,7 @@ int main(int argc, char *argv[])
 
         if (len < 0)
         {
-            if (errno == EINTR)
+            if (EINTR == errno)
             {
                 errno = 0;
                 continue;
@@ -41,5 +45,7 @@ int main(int argc, char *argv[])
 
         write(1, "OK", 2);
     }
+
+    dbi_shutdown();
     return 0;
 }
