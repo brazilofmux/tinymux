32cf259e471ec9c13ec683a775b924b2be4555d0
rmg <rmg>
2006-01-06 07:02:11 +0000

    ensure first build is #1 rather than # with nothing after it

--- mux/src/buildnum.sh
+++ mux/src/buildnum.sh
@@ -4,7 +4,7 @@
 #
 PATH=/bin:/usr/bin:/usr/ucb
 #
-touch buildnum.data
-bnum=`awk '{ print $1 + 1 }' < buildnum.data`
+bnum=`cat buildnum.data 2>/dev/null`
+bnum=`expr "$bnum" + 1`
 echo $bnum > buildnum.data
 echo $bnum
