daf6268940b572563cc93533d996c7605887a919
rmg <rmg>
2006-01-06 07:16:33 +0000

    bsd expr may allow numeric empty string arguments, but others don't

--- mux/src/buildnum.sh
+++ mux/src/buildnum.sh
@@ -4,7 +4,7 @@
 #
 PATH=/bin:/usr/bin:/usr/ucb
 #
-bnum=`cat buildnum.data 2>/dev/null`
+bnum=`cat buildnum.data 2>/dev/null || echo 0`
 bnum=`expr "$bnum" + 1`
 echo $bnum > buildnum.data
 echo $bnum
