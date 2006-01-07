9e8eb2b8b6d00e2caf586d83838a0e75872db770
Brazil <brazilofmux@gmail.com>
2006-01-07 08:50:53 +0000

    Fix some of the Level 4 warnings in predicatescpp from the Intel Compiler.

--- mux/src/netcommon.cpp
+++ mux/src/netcommon.cpp
@@ -1,6 +1,6 @@
 // netcommon.cpp
 //
-// $Id: netcommon.cpp,v 1.60 2006/01/06 19:26:16 sdennis Exp $
+// $Id: netcommon.cpp,v 1.61 2006/01/07 08:50:53 sdennis Exp $
 //
 // This file contains routines used by the networking code that do not
 // depend on the implementation of the networking code.  The network-specific
@@ -26,8 +26,6 @@
 #ifdef REALITY_LVLS
 #include "levels.h"
 #endif /* REALITY_LVLS */
-
-extern void handle_prog(DESC *, char *);
 
 #ifdef WIN32
 extern HANDLE CompletionPort;    // IOs are queued up on this port
