e9d16ce6fc874260074eeca6d0f4648895dc1b0d
jake <jake>
2006-01-07 05:07:00 +0000

    Fix all level 4 warnings for VC 2005.

--- mux/src/comsys.cpp
+++ mux/src/comsys.cpp
@@ -1,6 +1,6 @@
 // comsys.cpp
 //
-// $Id: comsys.cpp,v 1.39 2006-01-06 22:08:46 sdennis Exp $
+// $Id: comsys.cpp,v 1.40 2006-01-07 05:07:00 jake Exp $
 //
 #include "copyright.h"
 #include "autoconf.h"
@@ -1077,8 +1077,8 @@ void SendChannelMessage
         int aflags;
         int logmax = DFLT_MAX_LOG;
         char *maxbuf;
-        ATTR *pattr;
-        if (  (pattr = atr_str("MAX_LOG"))
+        ATTR *pattr = atr_str("MAX_LOG");
+        if (  pattr
            && pattr->number)
         {
             maxbuf = atr_get(obj, pattr->number, &aowner, &aflags);
@@ -1305,8 +1305,8 @@ void do_comlast(dbref player, struct channel *ch, int arg)
     int aflags;
     dbref obj = ch->chan_obj;
     int logmax = MAX_RECALL_REQUEST;
-    ATTR *pattr;
-    if (  (pattr = atr_str("MAX_LOG"))
+    ATTR *pattr = atr_str("MAX_LOG");
+    if (  pattr
        && (atr_get_info(obj, pattr->number, &aowner, &aflags)))
     {
         char *maxbuf = atr_get(obj, pattr->number, &aowner, &aflags);
@@ -1347,6 +1347,8 @@ void do_comlast(dbref player, struct channel *ch, int arg)
 
 bool do_chanlog(dbref player, char *channel, char *arg)
 {
+    UNUSED_PARAMETER(player);
+
     int value;
     if (  !*arg
        || !is_integer(arg, NULL)
@@ -1461,6 +1463,11 @@ void do_addcom
     char *arg2
 )
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(key);
+    UNUSED_PARAMETER(nargs);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -1573,6 +1580,10 @@ void do_addcom
 
 void do_delcom(dbref executor, dbref caller, dbref enactor, int key, char *arg1)
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(key);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -1684,6 +1695,10 @@ void do_delcomchannel(dbref player, char *channel, bool bQuiet)
 
 void do_createchannel(dbref executor, dbref caller, dbref enactor, int key, char *channel)
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(key);
+
     if (select_channel(channel))
     {
         raw_notify(executor, tprintf("Channel %s already exists.", channel));
@@ -1766,6 +1781,10 @@ void do_createchannel(dbref executor, dbref caller, dbref enactor, int key, char
 
 void do_destroychannel(dbref executor, dbref caller, dbref enactor, int key, char *channel)
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(key);
+
     struct channel *ch;
     int j;
 
@@ -1938,6 +1957,10 @@ void do_comtitle
     char *arg2
 )
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(nargs);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -2003,6 +2026,10 @@ void do_comlist
     char* pattern
 )
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(key);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -2078,6 +2105,8 @@ void do_channelnuke(dbref player)
 
 void do_clearcom(dbref executor, dbref caller, dbref enactor, int unused2)
 {
+    UNUSED_PARAMETER(unused2);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -2094,6 +2123,10 @@ void do_clearcom(dbref executor, dbref caller, dbref enactor, int unused2)
 
 void do_allcom(dbref executor, dbref caller, dbref enactor, int key, char *arg1)
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(key);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -2144,6 +2177,10 @@ void sort_users(struct channel *ch)
 
 void do_channelwho(dbref executor, dbref caller, dbref enactor, int key, char *arg1)
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(key);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -2371,6 +2408,10 @@ void do_editchannel
     char *arg2
 )
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(nargs);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -2599,6 +2640,10 @@ void do_cemit
     char *text
 )
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(nargs);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -2639,6 +2684,8 @@ void do_chopen
     char *value
 )
 {
+    UNUSED_PARAMETER(nargs);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -2752,6 +2799,10 @@ void do_chboot
     char *victim
 )
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(nargs);
+
     // I sure hope it's not going to be that long.
     //
     if (!mudconf.have_comsys)
@@ -2887,6 +2938,9 @@ void do_chanlist
     char *pattern
 )
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+
     if (!mudconf.have_comsys)
     {
         raw_notify(executor, "Comsys disabled.");
@@ -3008,6 +3062,12 @@ void do_chanlist
 //
 FUNCTION(fun_comtitle)
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(nfargs);
+    UNUSED_PARAMETER(cargs);
+    UNUSED_PARAMETER(ncargs);
+
     if (!mudconf.have_comsys)
     {
         safe_str("#-1 COMSYS DISABLED", buff, bufc);
@@ -3086,6 +3146,12 @@ FUNCTION(fun_comtitle)
 //
 FUNCTION(fun_comalias)
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(nfargs);
+    UNUSED_PARAMETER(cargs);
+    UNUSED_PARAMETER(ncargs);
+
     if (!mudconf.have_comsys)
     {
         safe_str("#-1 COMSYS DISABLED", buff, bufc);
@@ -3140,6 +3206,11 @@ FUNCTION(fun_comalias)
 //
 FUNCTION(fun_channels)
 {
+    UNUSED_PARAMETER(caller);
+    UNUSED_PARAMETER(enactor);
+    UNUSED_PARAMETER(cargs);
+    UNUSED_PARAMETER(ncargs);
+
     if (!mudconf.have_comsys)
     {
         safe_str("#-1 COMSYS DISABLED", buff, bufc);
