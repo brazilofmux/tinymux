f3cf491b0f46a79a921119dd4ba28d3532ef9da7
Brazil <brazilofmux@gmail.com>
2006-08-04 03:21:14 +0000

    2.6.0.2

--- mux/src/_build.h
+++ mux/src/_build.h
@@ -1,6 +1,6 @@
 // _build.h
 //
-// $Id: _build.h,v 1.55 2006-02-03 07:45:19 sdennis Exp $
+// $Id: _build.h,v 1.56 2006-08-04 03:21:14 sdennis Exp $
 //
 #ifndef MUX_BUILD_NUM
 extern char szBuildNum[];
@@ -12,8 +12,8 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.6.0.1"          // Version number
-#define MUX_RELEASE_DATE  "2006-FEB-02"      // Source release date
+#define MUX_VERSION       "2.6.0.2"          // Version number
+#define MUX_RELEASE_DATE  "2006-AUG-03"      // Source release date
 
 // Define if this release is qualified as ALPHA or BETA.
 //
