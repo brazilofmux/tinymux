ae9d9b6c0aec5f1c96ec1dea8a9eaeb12321b0be
Brazil <brazilofmux@gmail.com>
2006-08-16 23:00:43 +0000

    Add check and warning about renaming user attribute names to names which
    already exist.

--- mux/src/db_rw.cpp
+++ mux/src/db_rw.cpp
@@ -1,6 +1,6 @@
 // db_rw.cpp
 //
-// $Id: db_rw.cpp,v 1.28 2006-08-16 22:24:42 sdennis Exp $
+// $Id: db_rw.cpp,v 1.29 2006-08-16 23:00:43 sdennis Exp $
 //
 
 #include "copyright.h"
@@ -609,7 +609,7 @@ dbref db_read(FILE *f, int *db_format, int *db_version, int *db_flags)
                         ATTR *a = (ATTR *)hashfindLEN(pName, nName, &mudstate.attr_name_htab);
                         if (a)
                         {
-                            Log.tinyprintf(ENDLINE "Renaming conflicting user attribute, %s, to FIRAN_%s." ENDLINE, i);
+                            Log.tinyprintf(ENDLINE "Renaming conflicting user attribute, %s, to FIRAN_%s." ENDLINE, pName, pName);
                             char *p = alloc_lbuf("db_read");
                             char *q = p;
                             safe_str("FIRAN_", p, &q);
@@ -618,6 +618,12 @@ dbref db_read(FILE *f, int *db_format, int *db_version, int *db_flags)
 
                             pName = MakeCanonicalAttributeName(p, &nName, &bValid);
                             free_lbuf(p);
+                            
+                            a = vattr_find_LEN(pName, nName);
+                            if (a)
+                            {
+                                Log.tinyprintf(ENDLINE "ERROR: Renamed user attribute, %s, already exists." ENDLINE, pName);
+                            }
                         }
                     }
 
