1ef490c9c219a39e25be5e52d3b3f3a731c79a99
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-01-26 16:51:13 +0000

    -- Add protection to add(), and use a const global for the size of g_nDoubles.
    git-svn-id: https://tinymux.googlecode.com/svn/branches/dev_jake@1007 d1b986fa-651c-0410-a323-35a8662cf44d

--- mux/src/funmath.cpp
+++ mux/src/funmath.cpp
@@ -254,7 +254,8 @@ static const long nMaximums[10] =
     0, 9, 99, 999, 9999, 99999, 999999, 9999999, 99999999, 999999999
 };
 
-static double g_aDoubles[(LBUF_SIZE+1)/2];
+static double g_aDoubles[LBUF_SIZE];
+int const g_nDoubles = sizeof(g_aDoubles)/sizeof(double);
 
 FUNCTION(fun_add)
 {
@@ -265,8 +266,14 @@ FUNCTION(fun_add)
     UNUSED_PARAMETER(cargs);
     UNUSED_PARAMETER(ncargs);
 
+    int nArgs = nfargs;
+    if (g_nDoubles < nArgs)
+    {
+        nArgs = g_nDoubles;
+    }
+
     int i;
-    for (i = 0; i < nfargs; i++)
+    for (i = 0; i < nArgs; i++)
     {
         int nDigits;
         long nMaxValue = 0;
@@ -276,11 +283,12 @@ FUNCTION(fun_add)
         {
             // Do it the slow way.
             //
-            for (int j = 0; j < nfargs; j++)
+            for (int j = 0; j < nArgs; j++)
             {
                 g_aDoubles[j] = mux_atof(fargs[j]);
             }
-            fval(buff, bufc, AddDoubles(nfargs, g_aDoubles));
+
+            fval(buff, bufc, AddDoubles(nArgs, g_aDoubles));
             return;
         }
     }
@@ -288,7 +296,7 @@ FUNCTION(fun_add)
     // We can do it the fast way.
     //
     long sum = 0;
-    for (i = 0; i < nfargs; i++)
+    for (i = 0; i < nArgs; i++)
     {
         sum += mux_atol(fargs[i]);
     }
@@ -314,8 +322,8 @@ FUNCTION(fun_ladd)
         }
 
         char *cp = trim_space_sep(fargs[0], &sep);
-        while (  cp 
-              && n < (LBUF_SIZE+1)/2)
+        while (  cp
+              && n < g_nDoubles)
         {
             char *curr = split_token(&cp, &sep);
             g_aDoubles[n++] = mux_atof(curr);
