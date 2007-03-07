ff94a469ddad8488a7da2c103fa4c73afe1a3ecf
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-03-07 03:25:05 +0000

    -- Fix memory leak in process_cmdent().
    git-svn-id: https://tinymux.googlecode.com/svn/branches/TinyMUX-2.6@1240 d1b986fa-651c-0410-a323-35a8662cf44d

--- mux/src/command.cpp
+++ mux/src/command.cpp
@@ -1346,6 +1346,7 @@ static void process_cmdent(CMDENT *cmdp, char *switchp, dbref executor, dbref ca
                 }
                 if (!*s)
                 {
+                    free_lbuf(buff);
                     break;
                 }
                 *s++ = '\0';
