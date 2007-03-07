d50a896e5bfe8f7d2466145bad886aaf00aa259a
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-03-07 00:36:20 +0000

    -- Fix memory leak in @mail error code path.
    git-svn-id: https://tinymux.googlecode.com/svn/trunk@1238 d1b986fa-651c-0410-a323-35a8662cf44d

--- mux/src/mail.cpp
+++ mux/src/mail.cpp
@@ -3864,6 +3864,7 @@ static void do_expmail_stop(dbref player, int flags)
         if (*pMailMsg == '\0')
         {
             notify(player, "MAIL: The body of this message is empty.  Use - to add to the message.");
+            free_lbuf(tolist);
         }
         else
         {
