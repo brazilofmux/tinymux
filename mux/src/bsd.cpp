72f9cd39d36f57b535525456350500452bf244d2
duskwave <duskwave@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-03-15 19:02:32 +0000

    Use #if (_MSC_VER >= 1400) for sscanf_s.
    git-svn-id: https://tinymux.googlecode.com/svn/branches/dev_jake@1454 d1b986fa-651c-0410-a323-35a8662cf44d

--- mux/src/bsd.cpp
+++ mux/src/bsd.cpp
@@ -445,8 +445,13 @@ static int get_slave_result(void)
         }
     }
 
+#if (_MSC_VER >= 1400)
     if (sscanf_s((char *)ident, "%d , %d : %s : %s : %s", &remote_port, &local_port, 
                     token, MAX_STRING, os, MAX_STRING, userid, MAX_STRING) != 5)
+#else
+    if (sscanf((char *)ident, "%d , %d : %s : %s : %s", &remote_port, &local_port,
+                    token, os, userid, ) != 5)
+#endif
     {
         return 1;
     }
