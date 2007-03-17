69a3127c83fc4ae8c41b56795d309562abf3ac34
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2007-03-17 05:18:48 +0000

    -- Finish 2.6.2.21.
    git-svn-id: https://tinymux.googlecode.com/svn/branches/TinyMUX-2.6@1487 d1b986fa-651c-0410-a323-35a8662cf44d

--- mux/src/_build.h
+++ mux/src/_build.h
@@ -12,8 +12,8 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.6.2.20"         // Version number
-#define MUX_RELEASE_DATE  "2007-JAN-31"      // Source release date
+#define MUX_VERSION       "2.6.2.21"         // Version number
+#define MUX_RELEASE_DATE  "2007-MAR-17"      // Source release date
 
 // Define if this release is qualified as ALPHA or BETA.
 //
