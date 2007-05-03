755164bbcbc58aac0ab9528e70cb790587b31bfc
Brazil <brazilofmux@gmail.com>
2007-05-03 00:08:28 -0700

    -- Continue.

--- src/out.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-for i in $@
-do
-    j=`basename $i .c`.cpp
-    echo Renaming file $i to $j
-    mv $i $j
-done
--- src/sa-boolexp.c
+++
@@ -1,9 +0,0 @@
-/*
- * Standalone boolexp.c 
- */
-/*
- * $Id: sa-boolexp.c,v 1.2 1997/04/16 06:01:36 dpassmor Exp $ 
- */
-#undef MEMORY_BASED
-#define STANDALONE
-#include "boolexp.c"
