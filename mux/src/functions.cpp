dc90d27eb61dc27b4a75bec3d746aeeb00407d88
brazilofmux <brazilofmux@gmail.com>
2007-08-06 23:48:02 -0700

    -- Open permissions on WIDTH() and HEIGHT() (Issue 356).

--- mux/src/functions.cpp
+++ mux/src/functions.cpp
@@ -7231,11 +7231,7 @@ static FUNCTION(fun_height)
         dbref target = lookup_player(executor, pTargetName, true);
         if (Good_obj(target))
         {
-            if (  executor == target
-               || See_All(executor))
-            {
-                nHeight = fetch_height(target);
-            }
+            nHeight = fetch_height(target);
         }
     }
     safe_ltoa(nHeight, buff, bufc);
@@ -7275,11 +7271,7 @@ static FUNCTION(fun_width)
         dbref target = lookup_player(executor, pTargetName, true);
         if (Good_obj(target))
         {
-            if (  executor == target
-               || See_All(executor))
-            {
-                nWidth = fetch_width(target);
-            }
+            nWidth = fetch_width(target);
         }
     }
     safe_ltoa(nWidth, buff, bufc);
