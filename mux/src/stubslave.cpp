3aafda322e51fb70d33e245a8b7bd6a6b9ca2f82
brazilofmux <brazilofmux@gmail.com>
2007-09-12 08:10:36 -0700

    Add top-level loop, channel zero to stubslave.

--- mux/src/stubslave.cpp
+++ mux/src/stubslave.cpp
@@ -14,22 +14,70 @@
 #include "libmux.h"
 #include "modules.h"
 
-#define MAX_STRING 1000
-
-int main(int argc, char *argv[])
+typedef struct QueueBlock
 {
-    pid_t parent_pid = getppid();
-    if (parent_pid == 1)
+    void  *pBuffer;
+    size_t nBuffer;
+    struct QueueBlock *qbNext;
+    struct QueueBlock *qbPrev;
+} QUEUE_BLOCK;
+
+typedef struct
+{
+    QUEUE_BLOCK *pHead;
+    QUEUE_BLOCK *pTail;
+} QUEUE_INFO;
+
+QUEUE_INFO   *pQueue_In = NULL;
+QUEUE_INFO   *pQueue_Out = NULL;
+
+CHANNEL_INFO *aChannels = NULL;
+int           nChannels = 0;
+int           nChannelsAllocated = 0;
+
+#define CHANNELS_FIRST 100
+
+void Pipe_InitializeChannelZero(FCALL *pfCall0, FMSG *pfMsg0, FDISC *pfDisc0)
+{
+    try
     {
-        // Our real parent process is gone, and we have been inherited by the
-        // init process.
-        //
-        return 1;
+        aChannels = new CHANNEL_INFO[CHANNELS_FIRST];
+    }
+    catch (...)
+    {
+        ; // Nothing.
     }
 
-    // TODO: Should we pass in a pipepump function here or not?
-    //
-    mux_InitModuleLibrary(IsSlaveProcess, NULL);
+    if (NULL != aChannels)
+    {
+        nChannelsAllocated      = CHANNELS_FIRST;
+        aChannels[0].nChannel   = 0;
+        aChannels[0].pfCall     = pfCall0;
+        aChannels[0].pfMsg      = pfMsg0;
+        aChannels[0].pfDisc     = pfDisc0;
+        aChannels[0].pInterface = NULL;
+
+        nChannels = 1;
+    }
+}
+
+MUX_RESULT Channel0_Call(CHANNEL_INFO *pci, size_t nBuffer, void *pBuffer)
+{
+    return MUX_E_NOTIMPLEMENTED;
+}
+
+void Pipe_AddToQueue(QUEUE_INFO *pqi, size_t nBuffer, void *pBuffer)
+{
+}
+
+void Pipe_ParseQueue(QUEUE_INFO *pqi)
+{
+}
+
+#define MAX_STRING 1000
+
+void Stub_ShoveChars(int fdServer)
+{
     for (;;)
     {
         char arg[MAX_STRING];
@@ -49,10 +97,16 @@ int main(int argc, char *argv[])
             break;
         }
 
-        arg[len] = '\0';
-
-        write(1, "OK", 2);
+        Pipe_AddToQueue(pQueue_In, len, arg);
+        Pipe_ParseQueue(pQueue_In);
     }
+}
+
+int main(int argc, char *argv[])
+{
+    mux_InitModuleLibrary(IsSlaveProcess, NULL);
+    Pipe_InitializeChannelZero(Channel0_Call, NULL, NULL);
+    Stub_ShoveChars(0);
     mux_FinalizeModuleLibrary();
     return 0;
 }
