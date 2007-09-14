b221186bf32d369ba8a569456cc588d82f55d9f2
brazilofmux <brazilofmux@gmail.com>
2007-09-14 08:40:03 -0700

    Fix SIGSEGV from using flag_access with a flagname but without permissions.

--- mux/src/flags.cpp
+++ mux/src/flags.cpp
@@ -1045,7 +1045,10 @@ CF_HAND(cf_flag_access)
     char *fstr = mux_strtok_parse(&tts);
     char *permstr = mux_strtok_parse(&tts);
 
-    if (!fstr || !*fstr)
+    if (  NULL == fstr
+       || '\0' == fstr[0]
+       || NULL == permstr
+       || '\0' == permstr[0])
     {
         return -1;
     }
