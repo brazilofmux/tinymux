590f136ce952a668aa4248b6ed7685659f1b89b0
brazilofmux <brazilofmux@gmail.com>
2008-05-09 08:01:16 -0700

    Untabify.

--- client/win/ritsuDoc.cpp
+++ client/win/ritsuDoc.cpp
@@ -18,17 +18,17 @@ static char THIS_FILE[] = __FILE__;
 IMPLEMENT_DYNCREATE(CRitsuDoc, CDocument)
 
 BEGIN_MESSAGE_MAP(CRitsuDoc, CDocument)
-	//{{AFX_MSG_MAP(CRitsuDoc)
-		// NOTE - the ClassWizard will add and remove mapping macros here.
-		//    DO NOT EDIT what you see in these blocks of generated code!
-	//}}AFX_MSG_MAP
+    //{{AFX_MSG_MAP(CRitsuDoc)
+        // NOTE - the ClassWizard will add and remove mapping macros here.
+        //    DO NOT EDIT what you see in these blocks of generated code!
+    //}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
 BEGIN_DISPATCH_MAP(CRitsuDoc, CDocument)
-	//{{AFX_DISPATCH_MAP(CRitsuDoc)
-		// NOTE - the ClassWizard will add and remove mapping macros here.
-		//      DO NOT EDIT what you see in these blocks of generated code!
-	//}}AFX_DISPATCH_MAP
+    //{{AFX_DISPATCH_MAP(CRitsuDoc)
+        // NOTE - the ClassWizard will add and remove mapping macros here.
+        //      DO NOT EDIT what you see in these blocks of generated code!
+    //}}AFX_DISPATCH_MAP
 END_DISPATCH_MAP()
 
 // Note: we add support for IID_IRitsu to support typesafe binding
@@ -40,7 +40,7 @@ static const IID IID_IRitsu =
 { 0xac4d49e5, 0xcc72, 0x422b, { 0x96, 0x43, 0x11, 0x4d, 0x9a, 0xbc, 0x25, 0x5d } };
 
 BEGIN_INTERFACE_MAP(CRitsuDoc, CDocument)
-	INTERFACE_PART(CRitsuDoc, IID_IRitsu, Dispatch)
+    INTERFACE_PART(CRitsuDoc, IID_IRitsu, Dispatch)
 END_INTERFACE_MAP()
 
 /////////////////////////////////////////////////////////////////////////////
@@ -48,27 +48,27 @@ END_INTERFACE_MAP()
 
 CRitsuDoc::CRitsuDoc()
 {
-	// TODO: add one-time construction code here
+    // TODO: add one-time construction code here
 
-	EnableAutomation();
+    EnableAutomation();
 
-	AfxOleLockApp();
+    AfxOleLockApp();
 }
 
 CRitsuDoc::~CRitsuDoc()
 {
-	AfxOleUnlockApp();
+    AfxOleUnlockApp();
 }
 
 BOOL CRitsuDoc::OnNewDocument()
 {
-	if (!CDocument::OnNewDocument())
-		return FALSE;
+    if (!CDocument::OnNewDocument())
+        return FALSE;
 
-	// TODO: add reinitialization code here
-	// (SDI documents will reuse this document)
+    // TODO: add reinitialization code here
+    // (SDI documents will reuse this document)
 
-	return TRUE;
+    return TRUE;
 }
 
 
@@ -78,9 +78,9 @@ BOOL CRitsuDoc::OnNewDocument()
 
 void CRitsuDoc::Serialize(CArchive& ar)
 {
-	// CEditView contains an edit control which handles all serialization.
+    // CEditView contains an edit control which handles all serialization.
     //
-	((CEditView*)m_viewList.GetHead())->SerializeRaw(ar);
+    ((CEditView*)m_viewList.GetHead())->SerializeRaw(ar);
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -89,12 +89,12 @@ void CRitsuDoc::Serialize(CArchive& ar)
 #ifdef _DEBUG
 void CRitsuDoc::AssertValid() const
 {
-	CDocument::AssertValid();
+    CDocument::AssertValid();
 }
 
 void CRitsuDoc::Dump(CDumpContext& dc) const
 {
-	CDocument::Dump(dc);
+    CDocument::Dump(dc);
 }
 #endif //_DEBUG
 
