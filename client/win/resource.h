396b6d328dc739eca1f1b4caf9fa83e93df4fbba
brazilofmux <brazilofmux@gmail.com>
2008-05-09 08:27:52 -0700

    Add class names for Input and Output window classes.

--- client/win/resource.h
+++ client/win/resource.h
@@ -3,35 +3,38 @@
 // Used by ritsu.rc
 //
 #define IDC_MYICON                      2
-#define IDD_RITSU_DIALOG                102
-#define IDD_ABOUTBOX                    103
+#define IDD_RITSU_DIALOG                101
+#define IDD_ABOUTBOX                    102
 #define IDS_APP_TITLE                   103
-#define IDM_ABOUT                       104
-#define IDM_EXIT                        105
-#define IDS_HELLO                       106
-#define IDI_RITSU                       107
-#define IDI_SMALL                       108
-#define IDS_RITSU                       109
-#define IDS_MAIN_FRAME                  110
-#define IDR_MAINFRAME                   111
-#define IDI_BACKSCROLL                  112
-#define IDM_FILE_NEW                    113
-#define IDM_FILE_CLOSE                  114
-#define IDM_FILE_OPEN                   115
-#define IDM_WINDOW_TILE                 116
-#define IDM_WINDOW_CASCADE              117
-#define IDM_WINDOW_ARRANGE              118
-#define IDM_WINDOW_CLOSE_ALL            119
-#define IDC_MAIN_MDI                    120
-#define IDS_SESSION_FRAME               122
-#define IDC_RITSU                       123
+#define IDS_HELLO                       104
+#define IDS_RITSU                       105
+#define IDR_MAINFRAME                   106
+#define IDM_FILE_NEW                    107
+#define IDM_FILE_CLOSE                  108
+#define IDM_FILE_OPEN                   109
+#define IDM_EXIT                        110
+#define IDM_WINDOW_TILE                 111
+#define IDM_WINDOW_CASCADE              112
+#define IDM_WINDOW_ARRANGE              113
+#define IDM_WINDOW_CLOSE_ALL            114
+#define IDM_ABOUT                       115
+#define IDC_MAIN_MDI                    116
+#define IDC_RITSU                       117
+#define IDS_MAIN_FRAME                  118
+#define IDS_SESSION_FRAME               119
+#define IDS_OUTPUT_FRAME                120
+#define IDS_INPUT_FRAME                 121
+#define IDI_RITSU                       122
+#define IDI_SMALL                       123
+#define IDI_BACKSCROLL                  124
+
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        124
+#define _APS_NEXT_RESOURCE_VALUE        125
 #define _APS_NEXT_COMMAND_VALUE         32775
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
