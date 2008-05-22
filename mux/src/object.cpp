645744fd1ff9e0348dbcb26319705b3d2b16704e
brazilofmux <brazilofmux@gmail.com>
2008-05-21 22:10:11 -0700

    Fix interaction between @notify and @destroy (Issue 435).

--- mux/src/object.cpp
+++ mux/src/object.cpp
@@ -404,7 +404,7 @@ void destroy_bad_obj(dbref obj)
     if (!mudstate.bStandAlone)
     {
         halt_que(NOTHING, obj);
-        nfy_que(obj, A_SEMAPHORE, NFY_DRAIN, 0);
+        nfy_que(obj, 0, NFY_DRAIN, 0);
         fwdlist_clr(obj);
         stack_clr(obj);
         ReleaseAllResources(obj);
@@ -455,7 +455,7 @@ void destroy_obj(dbref obj)
         {
             notify(owner, "Halted.");
         }
-        nfy_que(obj, A_SEMAPHORE, NFY_DRAIN, 0);
+        nfy_que(obj, 0, NFY_DRAIN, 0);
 
         // Remove forwardlists and stacks.
         //
