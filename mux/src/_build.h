3eac67ca43847403b87f358541917d18263a3375
brazilofmux <brazilofmux@gmail.com>
2008-05-22 08:25:10 -0700

    Bump build number.

--- mux/src/_build.h
+++ mux/src/_build.h
@@ -16,7 +16,7 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.3.6.31"     // Version number
+#define MUX_VERSION       "2.3.6.32"     // Version number
 #define MUX_RELEASE_DATE  "2006-NOV-17"  // Source release date
 
 // Define if this release is qualified as ALPHA or BETA.
