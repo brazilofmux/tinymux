e1c67d6791fdf2d8afa790078d15027343cac003
brazilofmux <brazilofmux@gmail.com>
2008-12-06 18:47:01 -0800

    Fix padding in SHA1_Final() for data 9 bytes shorter than a multiple of 64 (Issue 544).

--- mux/src/sha1.cpp
+++ mux/src/sha1.cpp
@@ -122,7 +122,7 @@ void SHA1_Compute(SHA1_CONTEXT *p, size_t n, const char *buf)
 void SHA1_Final(SHA1_CONTEXT *p)
 {
     p->block[p->nblock++] = 0x80;
-    if (sizeof(p->block) - sizeof(UINT64) <= p->nblock)
+    if (sizeof(p->block) - sizeof(UINT64) < p->nblock)
     {
         memset(p->block + p->nblock, 0, sizeof(p->block) - p->nblock);
         SHA1_HashBlock(p);
