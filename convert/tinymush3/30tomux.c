bf89cb3b2192d6e66fa4907a70b629e14deea390
brazilofmux <brazilofmux@gmail.com>
2009-02-15 22:49:55 -0800

    Add and reposition curly braces.

--- convert/tinymush3/30tomux.c
+++ convert/tinymush3/30tomux.c
@@ -1,14 +1,15 @@
 /* Original Code provided by Zenty (c) 1999, All rights reserved
-* Modified by Ashen-Shugar for attribute allignment and functionary locks
-* (c) 1999, All rights reserved
-*/
+ * Modified by Ashen-Shugar for attribute allignment and functionary locks
+ * (c) 1999, All rights reserved
+ */
 #include <stdio.h>
 #include <string.h>
 #define MUX_MASK1	0xFFFFFFFB
 #define MUX_MASK2	0xF707DFFF
 #define MUX_MASK3	0x00000000
 
-int main(void) {
+int main(void)
+{
     int val, flag1, flag2, flag3, nflag1, nflag2, nflag3, obj, ismux2;
     int mage, royalty, staff, ansi, immortal, offsetchk, badobj, cntr;
     char f[16384], *q, *f1, *f2, attrname[65];
@@ -23,8 +24,10 @@ int main(void) {
     gets(q);
     cntr = 1;
     obj = -1;
-    while(q != NULL && !feof(stdin) ) {
-        if(f[0] == '!') {
+    while (q != NULL && !feof(stdin))
+    {
+        if (f[0] == '!')
+        {
             obj=atoi(f1);
             /* object conversion */
             printf("%s\n",q);
@@ -36,25 +39,35 @@ int main(void) {
             gets(q); printf("%s\n",q); /* Link */
             gets(q); printf("%s\n",q); /* Next */
             gets(q);
-            if ( strchr(f, '/') == 0 && strchr(f, ':') == 0 ) {
+
+            if (strchr(f, '/') == 0 && strchr(f, ':') == 0)
+            {
                 printf("%s\n", q); /* Bool */
-            } else {
+            }
+            else
+            {
                 printf("%s\n", q); /* Functionary Lock */
                 gets(q);printf("%s\n",q);     /* Bool */
                 offsetchk = 1;
             }
+
             /* If previous was null, last was owner, not bool */
-            if ( (!( q && *q) && offsetchk) || !offsetchk ) {
+            if ((!( q && *q) && offsetchk) || !offsetchk)
+            {
                 gets(q); printf("%s\n",q); /* Owner */
             }
+
             offsetchk = 0;
             gets(q); printf("%s\n",q); /* Parent */
             gets(q); printf("%s\n",q); /* Pennies */
             /* flag conv */
             gets(q); flag1 = atoi(q);
-            if ( flag1 & 0x00000004 ) {
+
+            if (flag1 & 0x00000004)
+            {
                 fprintf(stderr, "WARNING: Unrecognized Object Type for #%d\r\n", obj);
             }
+
             gets(q); flag2 = atoi(q);
             gets(q); flag3 = atoi(q);
             nflag1 = (flag1 & MUX_MASK1);
@@ -66,98 +79,143 @@ int main(void) {
             fflush(stdout);
             fflush(stdout);
             gets(q);
-        } else
-            if(f[0] == '+') {
-                if((f[1] == 'A') || (f[1] == 'N')) {
+        }
+        else
+        {
+            if (f[0] == '+')
+            {
+                if ((f[1] == 'A') || (f[1] == 'N'))
+                {
                     val = atoi(f2);
-                    if ( (cntr == 3) )
+                    if ((cntr == 3))
+                    {
                         printf("-R1\n");
+                    }
                     printf("+%c%d\n",f[1],val);
                     cntr++;
                     fflush(stdout);
                     gets(q);
-                } else if(f[1] == 'X') {
+                }
+                else if (f[1] == 'X')
+                {
                     printf("%s\n", q);
                     cntr++;
                     fflush(stdout);
                     gets(q);
-                } else if(f[1] == 'T') {
+                }
+                else if (f[1] == 'T')
+                {
                     printf("+X992001\n");
                     cntr++;
                     fflush(stdout);
                     gets(q);
-                } else {
+                }
+                else
+                {
                     printf("%s\n",q);
                     fflush(stdout);
                     gets(q);
                 }
-            } else
-                if(f[0] == '>') {
+            }
+            else
+            {
+                if (f[0] == '>')
+                {
                     val = atoi(f1);
                     /* attr conversion */
-                    if((val == 99) || ((val > 212) && (val < 218))) {
-                        switch(val) {
-                                       case 99: strcpy(attrname, "ControlLock");
-                                           break;
-                                       case 213:strcpy(attrname, "NewObjs");
-                                           badobj++;
-                                           break;
-                                       case 214: /* Conformat */
-                                           break;
-                                       case 215: /* Exit Format */
-                                           break;
-                                       case 216:strcpy(attrname, "ExitTo");
-                                           break;
-                                       case 217:strcpy(attrname, "ChownLock");
-                                           break;
+                    if ((val == 99) || ((val > 212) && (val < 218)))
+                    {
+                        switch (val)
+                        {
+                        case 99:
+                            strcpy(attrname, "ControlLock");
+                            break;
+
+                        case 213:
+                            strcpy(attrname, "NewObjs");
+                            badobj++;
+                            break;
+
+                        case 214: /* Conformat */
+                            break;
+
+                        case 215: /* Exit Format */
+                            break;
+
+                        case 216:
+                            strcpy(attrname, "ExitTo");
+                            break;
+
+                        case 217:
+                            strcpy(attrname, "ChownLock");
+                            break;
                         }
-                        if ( val == 214 || val == 215 ) {
-                            if ( val == 214 ) {
+
+                        if (val == 214 || val == 215)
+                        {
+                            if (val == 214)
+                            {
                                 printf(">242\n", val);
-                            } else {
+                            }
+                            else
+                            {
                                 printf(">241\n", val);
                             }
                             fflush(stdout);
                             gets(q);
                             printf("%s\n",q);
                             gets(q);
-                        } else {
+                        }
+                        else
+                        {
                             gets(q);
-                            if ( val != 213 ) {
+                            if (val != 213)
+                            {
                                 fprintf(stderr, "Can not convert attribute '%s' (attrnum %d) for object #%d.\r\nAttribute will be skipped.\r\n", attrname, val, obj);
                                 fprintf(stderr, "--> Content of attribute: %s\r\n", q);
                             }
                             gets(q);
                         }
-                    } else  {
+                    }
+                    else 
+                    {
                         printf(">%d\n",val);
                         fflush(stdout);
                         gets(q);
                         printf("%s\n",q);
                         gets(q);
                     }
-                } else
-                    if(f[0] == '-') {
+                }
+                else
+                {
+                    if (f[0] == '-')
+                    {
                         printf("%s\n",q);
                         cntr++;
                         gets(q);
-                    } else {
+                    }
+                    else
+                    {
                         printf("%s\n",q);
                         fflush(stdout);
                         gets(q);
                     }
 
-                    if(strstr(f, "***END OF DUMP***") != NULL )
+                    if (strstr(f, "***END OF DUMP***") != NULL)
                     {
                         printf("***END OF DUMP***\n");
                         fflush(stdout);
                         q = NULL;
-                        if ( badobj > 0 ) {
+                        if (badobj > 0)
+                        {
                             fprintf(stderr, "WARNING: Can not convert INTERNAL attribute 'NewObjs'.\r\n");
                             fprintf(stderr, "--> %d total instances ignored.  Not required for MUX2.\r\n", badobj);
                         }
                         return(0);
                     }
+                }
+            }
+        }
     } /* while */
     return(0);	
 }
--- convert/tinymush3/muxcleaner.c
+++ convert/tinymush3/muxcleaner.c
@@ -1,8 +1,8 @@
 /* Coded by Ashen-Shugar (c) 1999, all rights reserved
-* This program strips quotes that TINY uses for attribute definition,
-* combines attributes that TINY tends to break up, and strips ^M's
-* that TINY tends to add as well.  How lame.
-*/
+ * This program strips quotes that TINY uses for attribute definition,
+ * combines attributes that TINY tends to break up, and strips ^M's
+ * that TINY tends to add as well.  How lame.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -16,56 +16,92 @@ main(int argc, char **argv)
         is_endline = 0,
         is_begobj = 0;
 
-    if ( argc < 3 ) {
+    if (argc < 3)
+    {
         printf("syntax: %s <filein> <fileout>\n", argv[0]);
         exit(1);
     }
-    if ( (fp_in = fopen(argv[1], "r")) == NULL ) {
+
+    if ((fp_in = fopen(argv[1], "r")) == NULL)
+    {
         printf("Error opening file %s for reading.\n", argv[1]);
         exit(1);
     }
-    if ( (fp_out = fopen(argv[2], "w")) == NULL ) {
+
+    if ((fp_out = fopen(argv[2], "w")) == NULL)
+    {
         printf("Error opening file %s for writing.\n", argv[2]);
         fclose(fp_in);
         exit(1);
     }
+
     is_endline=0;
     is_chklock=0;
-    while ( !feof(fp_in) ) {
+    while (!feof(fp_in))
+    {
         ch_in = getc(fp_in);
-        if ( ch_in == '\n' ) {
+        if (ch_in == '\n')
+        {
             is_endline=1;
         }
-        if ( ch_in == '!' && is_endline ) {
+
+        if (ch_in == '!' && is_endline)
+        {
             is_begobj=1;
         }
-        if ( is_endline && (ch_in == '>' || ch_in == '<') && is_begobj ) {
+
+        if (is_endline && (ch_in == '>' || ch_in == '<') && is_begobj)
+        {
             is_begobj=0;
             is_chklock=0;
         }
-        if ( ch_in != '\n' )
+
+        if (ch_in != '\n')
+        {
             is_endline=0;
-        if ( ch_in == '(' && is_begobj )
+        }
+
+        if (ch_in == '(' && is_begobj)
+        {
             is_chklock++;
-        if ( ch_in == ')' && is_begobj )
+        }
+
+        if (ch_in == ')' && is_begobj)
+        {
             is_chklock--;
-        if ( ch_in == '\013' ) {
+        }
+
+        if (ch_in == '\013')
+        {
             hard_return = 1;
             putc(ch_in, fp_out);
             continue;
         }
-        if ( (ch_in == '\r' || ch_in == '\n') && (hard_return || (is_chklock > 0)) ) {
+
+        if ((ch_in == '\r' || ch_in == '\n') && (hard_return || (is_chklock > 0)))
+        {
             hard_return = 0;
             continue;
         }
-        else if ( ch_in == '\0' )
+        else if (ch_in == '\0')
+        {
             hard_return = 0;
-        if ( !feof(fp_in) )
+        }
+
+        if (!feof(fp_in))
+        {
             putc(ch_in, fp_out);
+        }
     }
-    if ( fp_in )
+
+    if (fp_in)
+    {
         fclose(fp_in);
-    if ( fp_out )
+    }
+
+    if (fp_out)
+    {
         fclose(fp_out);
+    }
     return(0);
 }
