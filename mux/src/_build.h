d06e13b47457f4c8950e365dd8cb49ae094e5366
brazilofmux <brazilofmux@gmail.com>
2010-01-24 16:59:02 -0800

    Bump version number to 2.7.4.31.

--- mux/src/_build.h
+++ mux/src/_build.h
@@ -15,8 +15,8 @@ extern char szBuildDate[];
 #define MUX_BUILD_DATE szBuildDate
 #endif // MUX_BUILD_DATE
 
-#define MUX_VERSION       "2.7.4.30"         // Version number
-#define MUX_RELEASE_DATE  "2009-DEC-04"      // Source release date
+#define MUX_VERSION       "2.7.4.31"         // Version number
+#define MUX_RELEASE_DATE  "2009-JAN-24"      // Source release date
 
 // Define if this release is qualified as ALPHA or BETA.
 //
