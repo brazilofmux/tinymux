603ca1aa7f43d6bd39f9dfbf6ebcc6c0bfea955d
brazilofmux <brazilofmux@gmail.com>
2010-06-08 10:20:23 -0700

    Remove HAVE_LIBDL. Access to dlopen() is all that should matter.

--- mux/src/autoconf-win32.h
+++ mux/src/autoconf-win32.h
@@ -118,9 +118,6 @@
 /* Define to 1 if you have the `kqueue' function. */
 /* #undef HAVE_KQUEUE */
 
-/* Define to 1 if you have the `dl' library (-ldl). */
-/* #undef HAVE_LIBDL */
-
 /* Define to 1 if you have the `inet' library (-linet). */
 /* #undef HAVE_LIBINET */
 
