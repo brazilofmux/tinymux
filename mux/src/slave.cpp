f7128b5dfbeede2b3768c0411746647d210f10c2
brazilofmux <brazilofmux@gmail.com>
2012-02-26 15:15:27 -0800

    Enable IPv6 in slave process.

--- mux/src/slave.cpp
+++ mux/src/slave.cpp
@@ -73,10 +73,10 @@ int query(char *ip)
     //
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
-    hints.ai_family   = AF_INET;
+    hints.ai_family   = AF_UNSPEC;
     hints.ai_socktype = SOCK_STREAM;
     hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_V4MAPPED|AI_ADDRCONFIG;
+    hints.ai_flags = AI_ADDRCONFIG;
 
     struct addrinfo *servinfo;
     char host[MAX_STRING];
