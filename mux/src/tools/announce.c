501ca2a3819012467b8ce46a0f0bbadf6a3074b4
brazilofmux <brazilofmux@gmail.com>
2012-02-26 19:16:45 -0700

    Update announcer to listen on both IPv4 and IPv6 addresses.

--- mux/src/tools/announce.c
+++ mux/src/tools/announce.c
@@ -9,58 +9,105 @@
  *
  *      Usage:  announce [port] < message_file
  *
- *      Author: Lawrence Brown <lpb@cs.adfa.oz.au>      Aug 90
- *
- *      Bits of code are adapted from the Berkeley telnetd sources
  *
  * \endverbatim
  */
 
-#define PORT    2860
+#include "autoconf.h"
 
-#include <sys/param.h>
+#ifdef HAVE_SYS_SOCKET_H
 #include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/resource.h>
+#endif
+
+#ifdef HAVE_NETINET_IN_H
 #include <netinet/in.h>
+#endif
+
+#ifdef HAVE_ARPA_INET_H
 #include <arpa/inet.h>
+#endif
+
+#ifdef HAVE_NETDB_H
 #include <netdb.h>
+#endif
+
+#include <sys/resource.h>
+
 #include <signal.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
 
-char   *Name;        // name of this program for error messages.
+#define PORT    "2860"
+
 char    msg[8192];
 size_t  nmsg;
+char host_address[1024];
+
+void child(int s)
+{
+    setpriority(PRIO_PROCESS, getpid(), 10);
+
+    if (listen(s, SOMAXCONN) < 0)
+    {
+        perror("announce: listen");
+        exit(1);
+    }
+
+    for (;;)
+    {
+        // loop forever, accepting requests & printing msg.
+        //
+#if defined(HAVE_SOCKADDR_IN6)
+        struct sockaddr_in6 sai;
+#else
+        struct sockaddr_in sai;
+#endif
+        socklen_t bar = sizeof(sai);
+        int ns = accept(s, (struct sockaddr *)&sai, &bar);
+        if (ns < 0)
+        {
+            perror("announce: accept");
+            _exit(1);
+        }
+
+        if (0 == getnameinfo((struct sockaddr *)&sai, bar, host_address, sizeof(host_address), NULL, 0, NI_NUMERICHOST|NI_NUMERICSERV))
+        {
+            time_t ct = time(0L);
+            fprintf(stderr, "CONNECTION made from %s at %s", host_address, ctime(&ct));
+            write(ns, msg, nmsg);
+            sleep(5);
+            close(ns);
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    static struct sockaddr_in sin = {AF_INET};
-
-    // Save program name for error messages.
-    //
-    Name = argv[0];
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+#if defined(HAVE_SOCKADDR_IN6)
+    hints.ai_family = AF_UNSPEC;
+#else
+    hints.ai_family = AF_INET;
+#endif
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    hints.ai_flags = AI_PASSIVE;
 
     // Assume PORT, but let command-line override.
     //
-    unsigned short usPort = PORT;
+    const char *sPort = PORT;
     argc--;
     argv++;
     if (argc > 0)
     {
         // unless specified on command-line.
         //
-        int iPort = atoi(*argv);
-        if (0 <= iPort && iPort <= 65535)
-        {
-            usPort = (unsigned short)iPort;
-        }
+        sPort = argv[0];
     }
-    sin.sin_port = htons(usPort);
 
     // Read in message and translate CRLF/NL to something reasonable.
     //
@@ -82,60 +129,56 @@ int main(int argc, char *argv[])
     nmsg = p - msg;
 
     signal(SIGHUP, SIG_IGN);
-    int s = socket(AF_INET, SOCK_STREAM, 0);
-    if (s < 0)
-    {
-        perror("announce: socket");
-        exit(1);
-    }
 
-    int opt = 1;
-    if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
+    struct addrinfo *ai;
+    struct addrinfo *servinfo;
+    if (0 == getaddrinfo(NULL, sPort, &hints, &servinfo))
     {
-        perror("setsockopt");
-    }
-
-    if (bind(s, (struct sockaddr *)&sin, sizeof sin) < 0)
-    {
-        perror("bind");
-        exit(1);
-    }
-
-    int foo;
-    if ((foo = fork()) != 0)
-    {
-        fprintf(stderr, "announce: pid %d running on port %d\n", foo, usPort);
-        _exit(0);
-    }
-    else
-    {
-        setpriority(PRIO_PROCESS, getpid(), 10);
-    }
-
-    if (listen(s, 1) < 0)
-    {
-        // start listening on port.
-        //
-        perror("announce: listen");
-        _exit(1);
-    }
-
-    for (;;)
-    {
-        // loop forever, accepting requests & printing msg.
-        //
-        socklen_t bar = sizeof(sin);
-        int ns = accept(s, (struct sockaddr *)&sin, &bar);
-        if (ns < 0)
+        for (ai = servinfo; NULL != ai; ai = ai->ai_next)
         {
-            perror("announce: accept");
-            _exit(1);
+            int s = socket(ai->ai_family, ai->ai_socktype, ai->ai_protocol);
+            if (s < 0)
+            {
+                perror("announce: socket");
+                exit(1);
+            }
+
+            int opt = 1;
+            if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
+            {
+                perror("SO_REUSEADDR");
+            }
+
+#if defined(HAVE_SOCKADDR_IN6)
+            if (AF_INET6 == ai->ai_family)
+            {
+                opt = 1;
+                if (setsockopt(s, IPPROTO_IPV6, IPV6_V6ONLY, (char *)&opt, sizeof(opt)) < 0)
+                {
+                    perror("IPV6_V6ONLY");
+                }
+            }
+#endif
+
+            if (bind(s, ai->ai_addr, ai->ai_addrlen) < 0)
+            {
+                perror("bind");
+                exit(1);
+            }
+
+            int foo = fork();
+            if (foo == -1)
+            {
+                perror("fork");
+                exit(1);
+            }
+            else if (0 == foo)
+            {
+                freeaddrinfo(servinfo);
+                child(s);
+            }
+
         }
-        char *host = inet_ntoa(sin.sin_addr);
-        long ct = time(0L);
-        fprintf(stderr, "CONNECTION made from %s at %s", host, ctime(&ct));
-        write(ns, msg, nmsg);
-        sleep(5);
-        close(ns);
+        freeaddrinfo(servinfo);
     }
 }
