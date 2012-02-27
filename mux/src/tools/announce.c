11991f3e134aa34c979fb1e591b36b17a75d9811
brazilofmux <brazilofmux@gmail.com>
2012-02-26 16:19:12 -0800

    Update announce.c so that it compiles on Linux.

--- mux/src/tools/announce.c
+++ mux/src/tools/announce.c
@@ -12,6 +12,7 @@
  *      Author: Lawrence Brown <lpb@cs.adfa.oz.au>      Aug 90
  *
  *      Bits of code are adapted from the Berkeley telnetd sources
+ *
  * \endverbatim
  */
 
@@ -23,10 +24,14 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
 #include <signal.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 char   *Name;        // name of this program for error messages.
 char    msg[8192];
@@ -34,16 +39,7 @@ size_t  nmsg;
 
 int main(int argc, char *argv[])
 {
-    int    s;
-    int    ns;
-    int    foo;
     static struct sockaddr_in sin = {AF_INET};
-    char   *host;
-    char   *inet_ntoa();
-    long   ct;
-    int    ch;
-    char   *p;
-    int    opt;
 
     // Save program name for error messages.
     //
@@ -51,20 +47,25 @@ int main(int argc, char *argv[])
 
     // Assume PORT, but let command-line override.
     //
-    sin.sin_port = htons((u_short) PORT);
+    unsigned short usPort = PORT;
     argc--;
     argv++;
     if (argc > 0)
     {
         // unless specified on command-line.
         //
-        sin.sin_port = atoi(*argv);
-        sin.sin_port = htons((u_short) sin.sin_port);
+        int iPort = atoi(*argv);
+        if (0 <= iPort && iPort <= 65535)
+        {
+            usPort = (unsigned short)iPort;
+        }
     }
+    sin.sin_port = htons(usPort);
 
     // Read in message and translate CRLF/NL to something reasonable.
     //
-    p = msg;
+    int ch;
+    char *p = msg;
     while (  (ch = getchar()) != EOF
           && p + 2 < msg + sizeof(msg))
     {
@@ -81,32 +82,36 @@ int main(int argc, char *argv[])
     nmsg = p - msg;
 
     signal(SIGHUP, SIG_IGN);
-    s = socket(AF_INET, SOCK_STREAM, 0);
+    int s = socket(AF_INET, SOCK_STREAM, 0);
     if (s < 0)
     {
         perror("announce: socket");
         exit(1);
     }
-    opt = 1;
+
+    int opt = 1;
     if (setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
     {
         perror("setsockopt");
     }
+
     if (bind(s, (struct sockaddr *)&sin, sizeof sin) < 0)
     {
         perror("bind");
         exit(1);
     }
+
+    int foo;
     if ((foo = fork()) != 0)
     {
-        fprintf(stderr, "announce: pid %d running on port %d\n", foo,
-                ntohs((u_short) sin.sin_port));
+        fprintf(stderr, "announce: pid %d running on port %d\n", foo, usPort);
         _exit(0);
     }
     else
     {
         setpriority(PRIO_PROCESS, getpid(), 10);
     }
+
     if (listen(s, 1) < 0)
     {
         // start listening on port.
@@ -114,24 +119,23 @@ int main(int argc, char *argv[])
         perror("announce: listen");
         _exit(1);
     }
-    foo = sizeof sin;
+
     for (;;)
     {
         // loop forever, accepting requests & printing msg.
         //
-        ns = accept(s, (struct sockaddr *)&sin, &foo);
+        socklen_t bar = sizeof(sin);
+        int ns = accept(s, (struct sockaddr *)&sin, &bar);
         if (ns < 0)
         {
             perror("announce: accept");
             _exit(1);
         }
-        host = inet_ntoa(sin.sin_addr);
-        ct = time(0L);
-        fprintf(stderr, "CONNECTION made from %s at %s",
-                host, ctime(&ct));
+        char *host = inet_ntoa(sin.sin_addr);
+        long ct = time(0L);
+        fprintf(stderr, "CONNECTION made from %s at %s", host, ctime(&ct));
         write(ns, msg, nmsg);
         sleep(5);
         close(ns);
     }
 }
-
