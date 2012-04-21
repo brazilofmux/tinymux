8d0772e3ae8d9f2f7f6d1befeac6ccb5427631d2
brazilofmux <brazilofmux@d1b986fa-651c-0410-a323-35a8662cf44d>
2012-04-21 04:06:26 +0000

    Fix crashbug in @reference.
    git-svn-id: https://tinymux.googlecode.com/svn/branches/dev_brazil@5546 d1b986fa-651c-0410-a323-35a8662cf44d

--- mux/src/predicates.cpp
+++ mux/src/predicates.cpp
@@ -3066,7 +3066,7 @@ void do_reference
     {
         try
         {
-            result = (reference_entry *)MEMALLOC(sizeof(result));
+            result = (reference_entry *)MEMALLOC(sizeof(reference_entry));
         }
         catch(...)
         {
