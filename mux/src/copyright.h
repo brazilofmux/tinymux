5a9d73d17b7b6da593ad7a4bb96cc8398c2793e6
brazilofmux <brazilofmux@gmail.com>
2012-07-12 10:08:02 -0700

    Fix minor niggles discovered while updating sources to 2.12.

--- mux/src/copyright.h
+++ mux/src/copyright.h
@@ -9,7 +9,7 @@
 /*
  * MUX Source Code (version 2.0 through 2.10).
  * Copyright (C) 1999-2004 by Solid Vertical Domains, Ltd.
- * Copyright (C) 2005-2010 Stephen Dennis
+ * Copyright (C) 2005-2012 Stephen Dennis
  *
  * TinyMUX Source Code (versions 1.0 through 1.6)
  * Copyright (c) 1995, 1996, 1997, 1998, 1999, David Passmore.
