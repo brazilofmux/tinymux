5cdb2b0008e161738b28d1fe9f2ad84a0e2fb28f
sdennis <sdennis@d1b986fa-651c-0410-a323-35a8662cf44d>
2016-08-25 04:29:17 +0000

    Remove support for x86 build with and without Intel compiler.

--- mux/src/config.h
+++ mux/src/config.h
@@ -631,10 +631,6 @@ extern void *MemRealloc(void *p, size_t n, const char *f, int l);
 #define getrusage(x,p)   syscall(SYS_GETRUSAGE,x,p)
 #endif // hpux
 
-#if defined(__INTEL_COMPILER)
-extern "C" unsigned int __intel_cpu_indicator;
-#endif
-
 #if defined(HAVE_SETRLIMIT) && defined(RLIMIT_NOFILE)
 void init_rlimit(void);
 #endif // HAVE_SETRLIMIT RLIMIT_NOFILE
