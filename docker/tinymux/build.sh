71e3513b4d237538c34d126ec6404a20d76ddc59
Stephen Dennis <brazilofmux@gmail.com>
2019-02-18 00:57:04 +0000

    Update docker setup to deal with openssl 1.1

--- docker/tinymux/build.sh
+++ docker/tinymux/build.sh
@@ -2,8 +2,8 @@
 # Arrange build environment
 rm -rf build
 mkdir build
-cp mux-2.12.0.3.unix.tar.gz inside.sh build
+cp mux-2.12.0.7.unix.tar.gz inside.sh openssl.patch build
 # Build TinyMUX
 sudo docker run --rm -v ${PWD}/build:/build -w /build alpine:latest ./inside.sh
 # Build Image
-sudo docker build -t tinymux:2.12.0.3 .
+sudo docker build -t tinymux:2.12.0.7 .
--- docker/tinymux/inside.sh
+++ docker/tinymux/inside.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 apk add --no-cache g++ make openssl openssl-dev
-tar xzf mux-2.12.0.3.unix.tar.gz
-cd mux2.12/src
+tar xzf mux-2.12.0.7.unix.tar.gz
+cd mux2.12
+patch -p2 < ../openssl.patch
+cd src
 ./configure --enable-ssl
 sed -e '80s/-lssl/-lssl -lcrypto/g' Makefile > Makefile.new
 mv Makefile.new Makefile
