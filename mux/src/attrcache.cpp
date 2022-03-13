3b530f88cde5c885e242dc5b99e6c31199ccd698
Stephen Dennis <brazilofmux@gmail.com>
2022-03-12 17:42:29 -0700

    Accept Resharper changes to attrcache.cpp.

--- mux/src/attrcache.cpp
+++ mux/src/attrcache.cpp
@@ -21,42 +21,41 @@ static CHashFile hfAttributeFile;
 static bool cache_initted = false;
 
 static bool cache_redirected = false;
-#define N_TEMP_FILES 8
-static FILE *TempFiles[N_TEMP_FILES];
+#define NUMBER_OF_TEMPORARY_FILES 8
+static FILE *temporary_files[NUMBER_OF_TEMPORARY_FILES];
 
 CLinearTimeAbsolute cs_ltime;
 
 #pragma pack(1)
-typedef struct tagAttrRecord
+struct attribute_record
 {
     Aname attrKey;
     UTF8 attrText[LBUF_SIZE];
-} ATTR_RECORD, *PATTR_RECORD;
+};
 #pragma pack()
 
-static ATTR_RECORD TempRecord;
+static attribute_record temp_record;
 
-typedef struct tagCacheEntryHeader
+struct cache_entry_header
 {
-    struct tagCacheEntryHeader *pPrevEntry;
-    struct tagCacheEntryHeader *pNextEntry;
+    struct cache_entry_header* previous_cache_entry;
+    struct cache_entry_header* next_cache_entry;
     Aname attrKey;
     size_t nSize;
-} CENT_HDR, *PCENT_HDR;
+};
 
-static PCENT_HDR pCacheHead = 0;
-static PCENT_HDR pCacheTail = 0;
-static size_t CacheSize = 0;
+static struct cache_entry_header* cache_head = nullptr;
+static struct cache_entry_header* cache_trail = nullptr;
+static size_t cache_size = 0;
 
-int cache_init(const UTF8 *game_dir_file, const UTF8 *game_pag_file,
-    int nCachePages)
+int cache_init(_In_z_ const UTF8 *game_dir_file, _In_z_ const UTF8 *game_pag_file, int nCachePages)
 {
     if (cache_initted)
     {
         return HF_OPEN_STATUS_ERROR;
     }
 
-    int cc = hfAttributeFile.Open(game_dir_file, game_pag_file, nCachePages);
+    const int cc = hfAttributeFile.Open(game_dir_file, game_pag_file, nCachePages);
     if (cc != HF_OPEN_STATUS_ERROR)
     {
         // Mark caching system live
@@ -69,40 +68,40 @@ int cache_init(const UTF8 *game_dir_file, const UTF8 *game_pag_file,
 
 void cache_redirect(void)
 {
-    for (int i = 0; i < N_TEMP_FILES; i++)
+    for (int i = 0; i < NUMBER_OF_TEMPORARY_FILES; i++)
     {
-        UTF8 TempFileName[20];
-        mux_sprintf(TempFileName, sizeof(TempFileName), T("$convtemp.%d"), i);
-        mux_assert(mux_fopen(&TempFiles[i], TempFileName, T("wb+")));
-        mux_assert(TempFiles[i]);
-        setvbuf(TempFiles[i], nullptr, _IOFBF, 16384);
+        UTF8 temporary_file_name[20];
+        mux_sprintf(temporary_file_name, sizeof(temporary_file_name), T("convtemp.%d"), i);
+        mux_assert(mux_fopen(&temporary_files[i], temporary_file_name, T("wb+")));
+        mux_assert(temporary_files[i]);
+        setvbuf(temporary_files[i], nullptr, _IOFBF, 16384);
     }
     cache_redirected = true;
 }
 
 void cache_pass2(void)
 {
-    ATTR_RECORD Record;
+    attribute_record record{};
     cache_redirected = false;
     mux_fprintf(stderr, T("2nd Pass:\n"));
-    for (int i = 0; i < N_TEMP_FILES; i++)
+    for (int i = 0; i < NUMBER_OF_TEMPORARY_FILES; i++)
     {
         mux_fprintf(stderr, T("File %d: "), i);
-        long int li = fseek(TempFiles[i], 0, SEEK_SET);
+        const long int li = fseek(temporary_files[i], 0, SEEK_SET);
         mux_assert(0L == li);
 
         int cnt = 1000;
         size_t nSize;
         for (;;)
         {
-            size_t cc = fread(&nSize, 1, sizeof(nSize), TempFiles[i]);
+            size_t cc = fread(&nSize, 1, sizeof(nSize), temporary_files[i]);
             if (cc != sizeof(nSize))
             {
                 break;
             }
-            cc = fread(&Record, 1, nSize, TempFiles[i]);
+            cc = fread(&record, 1, nSize, temporary_files[i]);
             mux_assert(cc == nSize);
-            cache_put(&Record.attrKey, Record.attrText, nSize - sizeof(Aname));
+            cache_put(&record.attrKey, record.attrText, nSize - sizeof(Aname));
             if (cnt-- == 0)
             {
                 fputc('.', stderr);
@@ -110,22 +109,22 @@ void cache_pass2(void)
                 cnt = 1000;
             }
         }
-        fclose(TempFiles[i]);
-        UTF8 TempFileName[20];
-        mux_sprintf(TempFileName, sizeof(TempFileName), T("$convtemp.%d"), i);
-        RemoveFile(TempFileName);
+        fclose(temporary_files[i]);
+        UTF8 temporary_file_name[20];
+        mux_sprintf(temporary_file_name, sizeof(temporary_file_name), T("convtemp.%d"), i);
+        RemoveFile(temporary_file_name);
         mux_fprintf(stderr, T(ENDLINE));
     }
 }
 
 void cache_cleanup(void)
 {
-    for (int i = 0; i < N_TEMP_FILES; i++)
+    for (int i = 0; i < NUMBER_OF_TEMPORARY_FILES; i++)
     {
-        fclose(TempFiles[i]);
-        UTF8 TempFileName[20];
-        mux_sprintf(TempFileName, sizeof(TempFileName), T("$convtemp.%d"), i);
-        RemoveFile(TempFileName);
+        fclose(temporary_files[i]);
+        UTF8 temporary_file_name[20];
+        mux_sprintf(temporary_file_name, sizeof(temporary_file_name), T("convtemp.%d"), i);
+        RemoveFile(temporary_file_name);
     }
 }
 
@@ -140,74 +139,74 @@ void cache_tick(void)
     hfAttributeFile.Tick();
 }
 
-static void REMOVE_ENTRY(PCENT_HDR pEntry)
+static void remove_entry_from_list(_In_ cache_entry_header* entry)
 {
     // How is X positioned?
     //
-    if (pEntry == pCacheHead)
+    if (entry == cache_head)
     {
-        if (pEntry == pCacheTail)
+        if (entry == cache_trail)
         {
             // HEAD --> X --> 0
             //    0 <--  <-- TAIL
             //
-            // ASSERT: pEntry->pNextEntry == 0;
-            // ASSERT: pEntry->pPrevEntry == 0;
+            // ASSERT: entry->next_cache_entry == 0;
+            // ASSERT: entry->previous_cache_entry == 0;
             //
-            pCacheHead = pCacheTail = 0;
+            cache_head = cache_trail = nullptr;
         }
         else
         {
             // HEAD  --> X --> Y --> 0
             //    0 <--   <--   <--  TAIL
             //
-            // ASSERT: pEntry->pNextEntry != 0;
-            // ASSERT: pEntry->pPrevEntry == 0;
+            // ASSERT: entry->next_cache_entry != 0;
+            // ASSERT: entry->previous_cache_entry == 0;
             //
-            pCacheHead = pEntry->pNextEntry;
-            pCacheHead->pPrevEntry = 0;
-            pEntry->pNextEntry = 0;
+            cache_head = entry->next_cache_entry;
+            cache_head->previous_cache_entry = nullptr;
+            entry->next_cache_entry = nullptr;
         }
     }
-    else if (pEntry == pCacheTail)
+    else if (entry == cache_trail)
     {
         // HEAD  --> Y --> X --> 0
         //    0 <--   <--   <-- TAIL
         //
-        // ASSERT: pEntry->pNextEntry == 0;
-        // ASSERT: pEntry->pPrevEntry != 0;
+        // ASSERT: entry->next_cache_entry == 0;
+        // ASSERT: entry->previous_cache_entry != 0;
         //
-        pCacheTail = pEntry->pPrevEntry;
-        pCacheTail->pNextEntry = 0;
-        pEntry->pPrevEntry = 0;
+        cache_trail = entry->previous_cache_entry;
+        cache_trail->next_cache_entry = nullptr;
+        entry->previous_cache_entry = nullptr;
     }
     else
     {
         // HEAD  --> Y --> X --> Z --> 0
         //    0 <--   <--   <--   <-- TAIL
         //
-        // ASSERT: pEntry->pNextEntry != 0;
-        // ASSERT: pEntry->pNextEntry != 0;
+        // ASSERT: entry->next_cache_entry != 0;
+        // ASSERT: entry->next_cache_entry != 0;
         //
-        pEntry->pNextEntry->pPrevEntry = pEntry->pPrevEntry;
-        pEntry->pPrevEntry->pNextEntry = pEntry->pNextEntry;
-        pEntry->pNextEntry = 0;
-        pEntry->pPrevEntry = 0;
+        entry->next_cache_entry->previous_cache_entry = entry->previous_cache_entry;
+        entry->previous_cache_entry->next_cache_entry = entry->next_cache_entry;
+        entry->next_cache_entry = nullptr;
+        entry->previous_cache_entry = nullptr;
     }
 }
 
-static void ADD_ENTRY(PCENT_HDR pEntry)
+static void add_entry_to_list(cache_entry_header* pEntry)
 {
-    if (pCacheHead)
+    if (cache_head)
     {
-        pCacheHead->pPrevEntry = pEntry;
+        cache_head->previous_cache_entry = pEntry;
     }
-    pEntry->pNextEntry = pCacheHead;
-    pEntry->pPrevEntry = 0;
-    pCacheHead = pEntry;
-    if (!pCacheTail)
+    pEntry->next_cache_entry = cache_head;
+    pEntry->previous_cache_entry = 0;
+    cache_head = pEntry;
+    if (!cache_trail)
     {
-        pCacheTail = pCacheHead;
+        cache_trail = cache_head;
     }
 }
 
@@ -215,19 +214,19 @@ static void TrimCache(void)
 {
     // Check to see if the cache needs to be trimmed.
     //
-    while (CacheSize > mudconf.max_cache_size)
+    while (cache_size > mudconf.max_cache_size)
     {
         // Blow something away.
         //
-        PCENT_HDR pCacheEntry = pCacheTail;
+        cache_entry_header* pCacheEntry = cache_trail;
         if (!pCacheEntry)
         {
-            CacheSize = 0;
+            cache_size = 0;
             break;
         }
 
-        REMOVE_ENTRY(pCacheEntry);
-        CacheSize -= pCacheEntry->nSize;
+        remove_entry_from_list(pCacheEntry);
+        cache_size -= pCacheEntry->nSize;
         hashdeleteLEN(&(pCacheEntry->attrKey), sizeof(Aname),
             &mudstate.acache_htab);
         MEMFREE(pCacheEntry);
@@ -237,31 +236,30 @@ static void TrimCache(void)
 
 const UTF8 *cache_get(Aname *nam, size_t *pLen)
 {
-    if (  nam == (Aname *) 0
+    if (  nam == static_cast<Aname*>(nullptr)
        || !cache_initted)
     {
         *pLen = 0;
         return nullptr;
     }
 
-    PCENT_HDR pCacheEntry = nullptr;
+    struct cache_entry_header* pCacheEntry;
     if (!mudstate.bStandAlone)
     {
         // Check the cache, first.
         //
-        pCacheEntry = (PCENT_HDR)hashfindLEN(nam, sizeof(Aname),
-            &mudstate.acache_htab);
+        pCacheEntry = static_cast<struct cache_entry_header*>(hashfindLEN(nam, sizeof(Aname), &mudstate.acache_htab));
         if (pCacheEntry)
         {
             // It was in the cache, so move this entry to the head of the queue.
             // and return a pointer to it.
             //
-            REMOVE_ENTRY(pCacheEntry);
-            ADD_ENTRY(pCacheEntry);
-            if (sizeof(CENT_HDR) < pCacheEntry->nSize)
+            remove_entry_from_list(pCacheEntry);
+            add_entry_to_list(pCacheEntry);
+            if (sizeof(cache_entry_header) < pCacheEntry->nSize)
             {
-                *pLen = pCacheEntry->nSize - sizeof(CENT_HDR);
-                return (UTF8 *)(pCacheEntry+1);
+                *pLen = pCacheEntry->nSize - sizeof(cache_entry_header);
+                return reinterpret_cast<UTF8*>(pCacheEntry + 1);
             }
             else
             {
@@ -271,38 +269,38 @@ const UTF8 *cache_get(Aname *nam, size_t *pLen)
         }
     }
 
-    UINT32 nHash = CRC32_ProcessInteger2(nam->object, nam->attrnum);
+    const UINT32 nHash = CRC32_ProcessInteger2(nam->object, nam->attrnum);
     UINT32 iDir = hfAttributeFile.FindFirstKey(nHash);
 
     while (iDir != HF_FIND_END)
     {
         HP_HEAPLENGTH nRecord;
-        hfAttributeFile.Copy(iDir, &nRecord, &TempRecord);
+        hfAttributeFile.Copy(iDir, &nRecord, &temp_record);
 
-        if (  TempRecord.attrKey.attrnum == nam->attrnum
-           && TempRecord.attrKey.object == nam->object)
+        if (  temp_record.attrKey.attrnum == nam->attrnum
+           && temp_record.attrKey.object == nam->object)
         {
-            int nLength = nRecord - sizeof(Aname);
+	        const int nLength = static_cast<int>(nRecord - sizeof(Aname));
             *pLen = nLength;
             if (!mudstate.bStandAlone)
             {
                 // Add this information to the cache.
                 //
-                pCacheEntry = (PCENT_HDR)MEMALLOC(sizeof(CENT_HDR)+nLength);
+                pCacheEntry = static_cast<struct cache_entry_header*>(MEMALLOC(sizeof(cache_entry_header)+nLength));
                 if (pCacheEntry)
                 {
                     pCacheEntry->attrKey = *nam;
-                    pCacheEntry->nSize = nLength + sizeof(CENT_HDR);
-                    CacheSize += pCacheEntry->nSize;
-                    memcpy((char *)(pCacheEntry+1), TempRecord.attrText, nLength);
-                    ADD_ENTRY(pCacheEntry);
+                    pCacheEntry->nSize = nLength + sizeof(cache_entry_header);
+                    cache_size += pCacheEntry->nSize;
+                    memcpy((char *)(pCacheEntry+1), temp_record.attrText, nLength);
+                    add_entry_to_list(pCacheEntry);
                     hashaddLEN(nam, sizeof(Aname), pCacheEntry,
                         &mudstate.acache_htab);
 
                     TrimCache();
                 }
             }
-            return TempRecord.attrText;
+            return temp_record.attrText;
         }
         iDir = hfAttributeFile.FindNextKey(iDir, nHash);
     }
@@ -313,13 +311,13 @@ const UTF8 *cache_get(Aname *nam, size_t *pLen)
     {
         // Add this information to the cache.
         //
-        pCacheEntry = (PCENT_HDR)MEMALLOC(sizeof(CENT_HDR));
+        pCacheEntry = static_cast<struct cache_entry_header*>(MEMALLOC(sizeof(cache_entry_header)));
         if (pCacheEntry)
         {
             pCacheEntry->attrKey = *nam;
-            pCacheEntry->nSize = sizeof(CENT_HDR);
-            CacheSize += pCacheEntry->nSize;
-            ADD_ENTRY(pCacheEntry);
+            pCacheEntry->nSize = sizeof(cache_entry_header);
+            cache_size += pCacheEntry->nSize;
+            add_entry_to_list(pCacheEntry);
             hashaddLEN(nam, sizeof(Aname), pCacheEntry,
                 &mudstate.acache_htab);
 
@@ -352,25 +350,25 @@ bool cache_put(Aname *nam, const UTF8 *value, size_t len)
     }
 #endif // HAVE_WORKING_FORK
 
-    if (len > sizeof(TempRecord.attrText))
+    if (len > sizeof(temp_record.attrText))
     {
-        len = sizeof(TempRecord.attrText);
+        len = sizeof(temp_record.attrText);
     }
 
     // Removal from DB.
     //
-    UINT32 nHash = CRC32_ProcessInteger2(nam->object, nam->attrnum);
+    const UINT32 nHash = CRC32_ProcessInteger2(nam->object, nam->attrnum);
 
     if (cache_redirected)
     {
-        TempRecord.attrKey = *nam;
-        memcpy(TempRecord.attrText, value, len);
-        TempRecord.attrText[len-1] = '\0';
+        temp_record.attrKey = *nam;
+        memcpy(temp_record.attrText, value, len);
+        temp_record.attrText[len-1] = '\0';
 
-        int iFile = (N_TEMP_FILES-1) & (nHash >> 29);
-        size_t nSize = len+sizeof(Aname);
-        fwrite(&nSize, 1, sizeof(nSize), TempFiles[iFile]);
-        fwrite(&TempRecord, 1, nSize, TempFiles[iFile]);
+        const int iFile = (NUMBER_OF_TEMPORARY_FILES-1) & (nHash >> 29);
+        const size_t nSize = len + sizeof(Aname);
+        fwrite(&nSize, 1, sizeof(nSize), temporary_files[iFile]);
+        fwrite(&temp_record, 1, nSize, temporary_files[iFile]);
         return true;
     }
 
@@ -378,23 +376,23 @@ bool cache_put(Aname *nam, const UTF8 *value, size_t len)
     while (iDir != HF_FIND_END)
     {
         HP_HEAPLENGTH nRecord;
-        hfAttributeFile.Copy(iDir, &nRecord, &TempRecord);
+        hfAttributeFile.Copy(iDir, &nRecord, &temp_record);
 
-        if (  TempRecord.attrKey.attrnum == nam->attrnum
-           && TempRecord.attrKey.object  == nam->object)
+        if (  temp_record.attrKey.attrnum == nam->attrnum
+           && temp_record.attrKey.object  == nam->object)
         {
             hfAttributeFile.Remove(iDir);
         }
         iDir = hfAttributeFile.FindNextKey(iDir, nHash);
     }
 
-    TempRecord.attrKey = *nam;
-    memcpy(TempRecord.attrText, value, len);
-    TempRecord.attrText[len-1] = '\0';
+    temp_record.attrKey = *nam;
+    memcpy(temp_record.attrText, value, len);
+    temp_record.attrText[len-1] = '\0';
 
     // Insertion into DB.
     //
-    if (!hfAttributeFile.Insert((HP_HEAPLENGTH)(len+sizeof(Aname)), nHash, &TempRecord))
+    if (!hfAttributeFile.Insert(static_cast<HP_HEAPLENGTH>(len + sizeof(Aname)), nHash, &temp_record))
     {
         Log.tinyprintf(T("cache_put((%d,%d), \xE2\x80\x98%s\xE2\x80\x99, %u) failed" ENDLINE),
             nam->object, nam->attrnum, value, len);
@@ -404,31 +402,30 @@ bool cache_put(Aname *nam, const UTF8 *value, size_t len)
     {
         // Update cache.
         //
-        PCENT_HDR pCacheEntry = (PCENT_HDR)hashfindLEN(nam, sizeof(Aname),
-            &mudstate.acache_htab);
-        if (pCacheEntry)
+        auto cache_entry = static_cast<struct cache_entry_header*>(hashfindLEN(nam, sizeof(Aname), &mudstate.acache_htab));
+        if (cache_entry)
         {
             // It was in the cache, so delete it.
             //
-            REMOVE_ENTRY(pCacheEntry);
-            CacheSize -= pCacheEntry->nSize;
+            remove_entry_from_list(cache_entry);
+            cache_size -= cache_entry->nSize;
             hashdeleteLEN((char *)nam, sizeof(Aname), &mudstate.acache_htab);
-            MEMFREE(pCacheEntry);
-            pCacheEntry = nullptr;
+            MEMFREE(cache_entry);
+            cache_entry = nullptr;
         }
 
         // Add information about the new entry back into the cache.
         //
-        size_t nSizeOfEntry = sizeof(CENT_HDR) + len;
-        pCacheEntry = (PCENT_HDR)MEMALLOC(nSizeOfEntry);
-        if (pCacheEntry)
+        const size_t nSizeOfEntry = sizeof(cache_entry_header) + len;
+        cache_entry = static_cast<struct cache_entry_header*>(MEMALLOC(nSizeOfEntry));
+        if (cache_entry)
         {
-            pCacheEntry->attrKey = *nam;
-            pCacheEntry->nSize = nSizeOfEntry;
-            CacheSize += pCacheEntry->nSize;
-            memcpy((char *)(pCacheEntry+1), TempRecord.attrText, len);
-            ADD_ENTRY(pCacheEntry);
-            hashaddLEN(nam, sizeof(Aname), pCacheEntry,
+            cache_entry->attrKey = *nam;
+            cache_entry->nSize = nSizeOfEntry;
+            cache_size += cache_entry->nSize;
+            memcpy((char *)(cache_entry+1), temp_record.attrText, len);
+            add_entry_to_list(cache_entry);
+            hashaddLEN(nam, sizeof(Aname), cache_entry,
                 &mudstate.acache_htab);
 
             TrimCache();
@@ -445,7 +442,7 @@ bool cache_sync(void)
 
 // Delete this attribute from the database.
 //
-void cache_del(Aname *nam)
+void cache_del(_In_ Aname *nam)
 {
     if (  !nam
        || !cache_initted)
@@ -462,16 +459,16 @@ void cache_del(Aname *nam)
     }
 #endif // HAVE_WORKING_FORK
 
-    UINT32 nHash = CRC32_ProcessInteger2(nam->object, nam->attrnum);
+    const UINT32 nHash = CRC32_ProcessInteger2(nam->object, nam->attrnum);
     UINT32 iDir = hfAttributeFile.FindFirstKey(nHash);
 
     while (iDir != HF_FIND_END)
     {
         HP_HEAPLENGTH nRecord;
-        hfAttributeFile.Copy(iDir, &nRecord, &TempRecord);
+        hfAttributeFile.Copy(iDir, &nRecord, &temp_record);
 
-        if (  TempRecord.attrKey.attrnum == nam->attrnum
-           && TempRecord.attrKey.object == nam->object)
+        if (  temp_record.attrKey.attrnum == nam->attrnum
+           && temp_record.attrKey.object == nam->object)
         {
             hfAttributeFile.Remove(iDir);
         }
@@ -482,17 +479,16 @@ void cache_del(Aname *nam)
     {
         // Update cache.
         //
-        PCENT_HDR pCacheEntry = (PCENT_HDR)hashfindLEN(nam, sizeof(Aname),
-            &mudstate.acache_htab);
-        if (pCacheEntry)
+        auto cache_entry = static_cast<struct cache_entry_header*>(hashfindLEN(nam, sizeof(Aname), &mudstate.acache_htab));
+        if (cache_entry)
         {
             // It was in the cache, so delete it.
             //
-            REMOVE_ENTRY(pCacheEntry);
-            CacheSize -= pCacheEntry->nSize;;
+            remove_entry_from_list(cache_entry);
+            cache_size -= cache_entry->nSize;
             hashdeleteLEN((char *)nam, sizeof(Aname), &mudstate.acache_htab);
-            MEMFREE(pCacheEntry);
-            pCacheEntry = nullptr;
+            MEMFREE(cache_entry);
+            cache_entry = nullptr;
         }
     }
 }
--- mux/src/attrcache.h
+++ mux/src/attrcache.h
@@ -12,13 +12,12 @@ typedef struct Aname
     unsigned int    attrnum;
 } Aname;
 
-extern const UTF8 *cache_get(Aname *nam, size_t *pLen);
-extern bool cache_put(Aname *nam, const UTF8 *obj, size_t len);
-extern int  cache_init(const UTF8 *game_dir_file, const UTF8 *game_pag_file,
-    int nCachePages);
-extern void cache_close(void);
-extern void cache_tick(void);
-extern bool cache_sync(void);
-extern void cache_del(Aname *nam);
+const UTF8 *cache_get(Aname *nam, size_t *pLen);
+bool cache_put(Aname *nam, const UTF8 *obj, size_t len);
+int cache_init(_In_z_ const UTF8* game_dir_file, _In_z_ const UTF8* game_pag_file, int nCachePages);
+void cache_close(void);
+void cache_tick(void);
+bool cache_sync(void);
+void cache_del(_In_ Aname *nam);
 
 #endif // !_ATTRCACHE_H
