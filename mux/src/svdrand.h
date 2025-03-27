f6ab2d122920e8a23673d07ef5852306e8f2b2d6
Stephen Dennis <brazilofmux@gmail.com>
2025-03-27 01:35:34 -0600

    Remove unused prototype from svdrand.h

--- mux/src/svdrand.h
+++ mux/src/svdrand.h
@@ -1,15 +1,12 @@
 /*! \file svdrand.h
  * \brief Random Numbers.
  *
- * Random Numbers based on algorithms presented in "Numerical Recipes in C",
- * Cambridge Press, 1992.
  */
 
 #ifndef SVDRAND_H
 #define SVDRAND_H
 
 void SeedRandomNumberGenerator(void);
-double RandomFloat(double flLow, double flHigh);
 INT32 RandomINT32(INT32 lLow, INT32 lHigh);
 
 #endif // SVDRAND_H
